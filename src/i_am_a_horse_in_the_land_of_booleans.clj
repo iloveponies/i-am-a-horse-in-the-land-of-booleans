eb7b74842bfc3c94bd159a633cb76d2df872ed19
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-10-13 16:59:09 +0300

    testicommit

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,10 +1,18 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
 (defn fizzbuzz [n]
-  ":(")
+  (if (= (mod n 15) 0)
+	"gotcha!"
+    (if (= (mod n 3) 0)
+      "fizz"
+      (if (= (mod n 5) 0)
+        "buzz"
+        ""))))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn teen? [age]
   ":(")
@@ -14,5 +22,3 @@
 
 (defn generic-doublificate [x]
   ":(")
-
-; '_______'
