4fb14bdaffd7cc35f2518155f410f8f65da26e64
Juhana Laurinharju <fingerzam@gmail.com>
2012-10-17 16:37:19 +0300

    Added lazytest to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject structured-data "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]
-                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
+  :profiles {:dev {:dependencies [[midje "1.4.0"]
+                                  [com.stuartsierra/lazytest "1.2.3"]]
+                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+  :repositories {"stuart" "http://stuartsierra.com/maven2"})
+
