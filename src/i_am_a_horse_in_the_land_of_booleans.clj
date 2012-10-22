58547ec84f88c41406f40d55e22c21d52467da90
Janne Haapsaari <haaja@iki.fi>
2012-10-22 11:52:08 +0300

    Solutions for i_am_a_horse_in_the_land_of_booleans exercises

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,27 +1,47 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
 (defn my-boolean [x]
-  ":(")
+  (if (= x nil) false
+  (if (= x false) false
+  true)))
 
 (defn abs [x]
-  ":(")
+  (if (>= x 0) x
+  (* x -1)))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+ (cond
+  (divides? 15 n) "gotcha!"
+  (divides? 5 n) "buzz"
+  (divides? 3 n) "fizz"
+  :else ""))
 
 (defn teen? [age]
-  ":(")
+ (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
+
+(defn own-doublificate [x]
+ (* x 2))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (or (vector? x) (list? x)) (map own-doublificate x)
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (if (divides? 4 year)
+    (if (divides? 100 year)
+      (if (divides? 400 year)
+       true
+       false)
+    true)
+   false))
 
 ; '_______'
