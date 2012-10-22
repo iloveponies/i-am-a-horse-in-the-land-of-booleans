727cab49b4f32987d31b9023a71d2d52772f597e
Sami Saada <sami.saada@gmail.com>
2012-10-22 21:03:50 +0300

    refactored

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -26,7 +26,8 @@
   (cond
     (number? x) (* 2 x)
     (empty? x) nil
-    (or (list? x) (vector? x)) (* (count x) 2)
+    (or (list? x)
+        (vector? x)) (* (count x) 2)
     :else true))
 
 (defn leap-year? [year]
