5db94a0d4ddcf1aaf938c9162199aeacd5d760fc
Marko T Haanranta <mhaanran@dhp-18.cs.helsinki.fi>
2012-10-22 16:54:00 +0300

    second part done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,27 +1,43 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
 (defn my-boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* x -1) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== 0 (mod n divisor)) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond 
+	(== 0 (mod n 15)) "gotcha!"
+	(== 0 (mod n 5)) "buzz"
+	(== 0 (mod n 3)) "fizz"
+	:else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (cond   
+	(not (<= 13 age 19)) true
+	:else false
+	))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* x 2)
+	(empty? x) nil
+	(or (list? x) (vector? x)) (* 2 (count x))
+	:else true
+	))
 
 (defn leap-year? [year]
-  ":(")
+   (cond 
+	(== 0 (mod year 400)) true 
+	(== 0 (mod year 100)) false
+	(== 0 (mod year 4)) true
+	:else false	
+	))
 
 ; '_______'
