8556636ad9c9275532b2ac2ae279e2d41ef75aeb
jezberg <jerx@welho.com>
2012-10-22 13:14:07 +0300

    All excercises solved

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,27 +1,51 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
-(defn my-boolean [x]
-  ":(")
+(defn 
+  my-boolean 
+  "Copies the functionality of the built in boolean"
+  [x]
+  (if x true false)
+)
 
 (defn abs [x]
-  ":(")
+  (*(if (< x 0) -1 1) x)
+)
 
-(defn divides? [divisor n]
-  ":(")
+(defn 
+  divides? 
+  "Returns True if divisor divides n"
+  [divisor n]
+  (if (== (mod n divisor) 0) true false)
+)
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+     (divides? 15 n) "gotcha!"
+     (divides? 3 n) "fizz"
+     (divides? 5 n) "buzz"
+     :else ""
+  )
+)
 
-(defn teen? [age]
-  ":(")
+(defn 
+  teen? 
+  "Returns true if the age is between 13 and 19"
+  [age]
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+)
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+     (number? x) (* 2 x)
+     (empty? x) nil
+     (or (list? x) (vector? x)) (map (fn [y] (* 2 y)) x)
+     :else true
+   )
+)
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (or (divides? 400 year) (and (not (divides? 100 year)) (divides? 4 year)))
+)
