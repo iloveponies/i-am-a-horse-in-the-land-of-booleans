b1cbf2c850ba46c8e2f2a72d70e3a365e68e65d7
Toni O Judin <tonjudin@melkki.cs.helsinki.fi>
2012-10-22 23:59:13 +0300

    trolli

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -11,32 +11,37 @@
     (* x -1)
     x))
 
-
 (defn divides? [divisor n]
   (if (== (mod n divisor) 0)
    true
    false))
 
 (defn fizzbuzz [n]
-  (cond 
-   (divides? 3 n) "fizz"
-   (divides? 5 n) "buzz"
+  (cond
    (divides? 15 n) "gotcha!"
+   (divides? 5 n) "buzz"
+   (divides? 3 n) "fizz"
    :else ""))
 
 (defn teen? [age]
- (> 13 age 19))
+ (<= 13 age 19))
 
 (defn not-teen? [age]
-  (cond
-   (< age 13)
-   (> age 19))
-   :else false)
+  (or (< age 13) (> age 19)))
 
 (defn generic-doublificate [x]
-  ":(")
+ (cond 
+   (number? x) (+ x x)
+   (empty? x) nil
+   (list? x) (+ (count x) (count x))
+   (vector? x) (+ (count x) (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+ (cond
+ (divides? 400 year) true
+ (divides? 100 year) false
+  (divides? 4 year) true
+  :else false))
 
 ; '_______'
