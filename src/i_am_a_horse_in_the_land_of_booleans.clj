6a51dfe5d7eea24efde1ecc885fa320601dd582b
Lassi J Vapaakallio <lvapaaka@dhp-8.cs.helsinki.fi>
2012-10-22 12:23:53 +0300

    Horses done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,27 +1,52 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
 (defn my-boolean [x]
-  ":(")
+  (if x true false)
+)
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x
+  )
+)
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor))
+)
 
 (defn fizzbuzz [n]
-  ":(")
+	(cond 
+     (divides? 15 n) "gotcha!"
+     (divides? 3 n) "fizz"
+     (divides? 5 n) "buzz"
+     :else ""
+  )
+)
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19)
+)
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+)
 
 (defn generic-doublificate [x]
-  ":(")
+	(cond
+     (number? x) (+ x x)
+	 (list? x) 
+	 (vector? x) 
+     (empty? x) nil
+     :else true
+	)
+)
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (cond 
+     (divides? 400 year) true
+     (divides? 100 year) false
+     (divides? 4 year) true
+     :else false
+  )
+)
