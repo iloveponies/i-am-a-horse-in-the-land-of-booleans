b919b3ac9e927f056b573bc155e683519aee0553
Toni O Judin <tonjudin@melkki.cs.helsinki.fi>
2012-10-22 22:02:01 +0300

    hhv

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,22 +1,37 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
 (defn my-boolean [x]
-  ":(")
+ (if x
+ true
+ false))
+
 
 (defn abs [x]
-  ":(")
+    (if (<= x 0)
+    (* x -1)
+    x))
+
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0)
+   true
+   false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond 
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   (divides? 15 n) "gotcha!"
+   :else ""))
 
 (defn teen? [age]
-  ":(")
+ (> 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (cond
+   (< age 13)
+   (> age 19))
+   :else false)
 
 (defn generic-doublificate [x]
   ":(")
