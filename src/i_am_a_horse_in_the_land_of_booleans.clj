0f20f0d84d675ecbfa5b1146f2b085abf2c483f8
antti <antti@laptop.lan>
2012-10-23 16:20:11 +0300

    valmis

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,27 +1,48 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
 (defn my-boolean [x]
-  ":(")
+	(if x true false ))
 
 (defn abs [x]
-  ":(")
+	(if (>= x 0)
+		x
+		(* -1 x)
+	))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  	(cond 
+    	(divides? 15 n) "gotcha!"
+    	(divides? 5 n) "buzz"
+    	(divides? 3 n) "fizz"
+    	:else ""
+	))
 
 (defn teen? [age]
-  ":(")
+	(<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+	(not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  	(cond
+    	(number? x) (* x 2)
+    	(empty? x) nil
+    	(or (vector? x) (list? x)) (* 2 (count x))
+   		:else true
+  	))
 
 (defn leap-year? [year]
-  ":(")
-
+  	(if (divides? 4 year)
+    	(if (divides? 100 year)
+      		(if (divides? 400 year)
+        		true
+        		false
+        	)
+      	true
+      	)
+    false
+    ))
 ; '_______'
