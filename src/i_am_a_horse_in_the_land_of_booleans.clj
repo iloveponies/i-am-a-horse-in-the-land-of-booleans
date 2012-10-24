a05b1d91ff429bc0273a999a700f62ca0568dd9a
mignatiu <mika.ignatius@helsinki.fi>
2012-10-24 21:39:59 +0300

    second
    fixed

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,21 +1,21 @@
 (ns i-am-a-horse-in-the-land-of-booleans)
 
-(defn my-boolean [x] (boolean x))
+(defn my-boolean [x] (if x true false ))
 
 (defn abs [x] (if (< x 0) (- x) x ))
 
-(defn divides? [divisor n] (if (== 0 (mod n divisor)) true false))
+(defn divides? [divisor n] (== 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
   (cond 
-   (== 0 (mod n 3))	 "fizz"
-   (== 0 (mod n 5))	 "buzz"
-   (== 0 (mod n 15)) "gotcha!"
-   :else		     ""))	
+    (divides? 15 n) "gotcha!"
+    (divides? 5 n)  "buzz"
+    (divides? 3 n)  "fizz"
+    :else 			""))
 
 (defn teen? [age] (if (< 12 age 20) true false))
 
-(defn not-teen? [age] (if (and (teen? age) true )false true))
+(defn not-teen? [age] (if (not teen? age)))
 
 (defn generic-doublificate [n]
   (cond
