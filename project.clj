fb143189487320dd7bec0cae3d0fce0caeb63420
Jani Rahkola <jani.rahkola@gmail.com>
2013-03-31 18:18:21 +0300

    stable midje

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject structured-data "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5-RC1"]]
-                   :plugins [[lein-midje "3.0-RC1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.0.0"]]}})
 
