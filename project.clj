afad563cee2ba959faf3b0f345672e1fa5ed7827
Jani Rahkola <jani.rahkola@gmail.com>
2013-03-31 18:17:49 +0300

    stable midje

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject i-am-a-horse-in-the-land-of-booleans "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev
-             {:dependencies [[midje "1.5-RC1"]]
-              :plugins [[lein-midje "3.0-RC1"]]}})
+             {:dependencies [[midje "1.5.1"]]
+              :plugins [[lein-midje "3.0.0"]]}})
