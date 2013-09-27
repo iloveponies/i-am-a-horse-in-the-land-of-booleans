f5bbda35cc8364d643a324bd4bb206497bbd4210
Jani Rahkola <jani.rahkola@iki.fi>
2013-09-27 16:11:38 +0300

    midje-grader support

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject i-am-a-horse-in-the-land-of-booleans "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [midje-grader "0.1.0-SNAPSHOT"]]
   :profiles {:dev
              {:dependencies [[midje "1.5.1"]]
               :plugins [[lein-midje "3.0.0"]]}})
