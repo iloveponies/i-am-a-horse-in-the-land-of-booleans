cd8743da9a3d5b2cf84dd16c47c3744ceedbd3bb
Jani Rahkola <jani.rahkola@iki.fi>
2013-10-04 14:35:37 +0300

    midje grading

--- test/structured_data_test.clj
+++ test/structured_data_test.clj
@@ -2,46 +2,53 @@
   (:use structured-data
         midje.sweet))
 
-(facts "1 do-a-thing"
+(facts "do-a-thing" {:exercise 1
+                     :points 1}
   (do-a-thing 3) => 46656.0
   (do-a-thing 1) => 4.0
   (do-a-thing 0) => 1.0)
 
-(facts "2 spiff"
+(facts "spiff" {:exercise 2
+                :points 1}
   (spiff [1 2 3])         => 4
   (spiff [1 2 -34 4 5 6]) => -33)
 
-(facts "3 cutify"
+(facts "cutify" {:exercise 3
+                 :points 1}
   (cutify [])        => ["<3"]
   (cutify [1 2 3])   => [1 2 3 "<3"]
   (cutify ["a" "b"]) => ["a" "b" "<3"])
 
-(facts "4 spiff-destructuring"
+(facts "spiff-destructuring" {:exercise 4
+                              :points 1}
   (spiff-destructuring [1 2 3])         => 4
   (spiff-destructuring [1 2 -34 4 5 6]) => -33)
 
-(tabular
-  (facts "5 width"
-    (width (rectangle ?bottom-left ?top-right)) => ?width)
-  ?bottom-left ?top-right ?width
-  [1 1]        [5 1]      4
-  [1 1]        [1 1]      0
-  [3 1]        [10 4]     7
-  [-1 -1]      [0 0]      1
-  [-5 2]       [0 2]      5)
+(facts {:exercise 5
+        :points 1}
+  (tabular
+   (facts "width"
+     (width (rectangle ?bottom-left ?top-right)) => ?width)
+   ?bottom-left ?top-right ?width
+   [1 1]        [5 1]      4
+   [1 1]        [1 1]      0
+   [3 1]        [10 4]     7
+   [-1 -1]      [0 0]      1
+   [-5 2]       [0 2]      5)
+
+  (tabular
+   (facts "height"
+     (height (rectangle ?bottom-left ?top-right)) => ?height)
+   ?bottom-left ?top-right ?height
+   [1 1]        [5 1]      0
+   [-1 -1]      [0 0]      1
+   [2 -7]       [4 4]      11
+   [1 1]        [5 5]      4
+   [0 0]        [2 3]      3))
 
 (tabular
-  (facts "5 height"
-    (height (rectangle ?bottom-left ?top-right)) => ?height)
-  ?bottom-left ?top-right ?height
-  [1 1]        [5 1]      0
-  [-1 -1]      [0 0]      1
-  [2 -7]       [4 4]      11
-  [1 1]        [5 5]      4
-  [0 0]        [2 3]      3)
-
-(tabular
-  (facts "6 square?"
+  (facts "square?" {:exercise 6
+                    :points 1}
     (square? (rectangle ?bottom-left ?top-right)) => ?square?)
   ?bottom-left ?top-right ?square?
   [1 1]        [2 2]      true
@@ -54,7 +61,8 @@
   [-2 -3]      [1 1]      false)
 
 (tabular
-  (facts "7 area"
+  (facts "area" {:exercise 7
+                 :points 1}
     (area (rectangle ?bottom-left ?top-right)) => ?area)
   ?bottom-left ?top-right ?area
   [1 1]        [5 1]      0
@@ -65,7 +73,8 @@
   [3 1]        [10 4]     21)
 
 (tabular
-  (facts "8 contains-point?"
+  (facts "contains-point?" {:exercise 8
+                            :points 1}
     (contains-point? (rectangle ?bottom-left ?top-right)
                       ?point)
          => ?contains?)
@@ -89,7 +98,8 @@
   [-1 2]       [3 7]     [-5 5]  false)
 
 (tabular
-  (facts "9 contains-rectangle?"
+  (facts "contains-rectangle?" {:exercise 9
+                                :points 1}
     (contains-rectangle? (rectangle ?x1 ?y1)
                          (rectangle ?x2 ?y2)) => ?contains?)
   ?x1     ?y1     ?x2     ?y2     ?contains?
@@ -128,22 +138,26 @@
                       :authors [friedman, felleisen]}
       books [cities, wild-seed, embassytown, little-schemer]]
 
-  (facts "10 title-length"
+  (facts "title-length" {:exercise 10
+                         :points 1}
     (title-length cities)         => 21
     (title-length wild-seed)      => 9
     (title-length little-schemer) => 18)
   
-  (facts "11 author-count"
+  (facts "author-count" {:exercise 11
+                         :points 1}
     (author-count cities)         => 1
     (author-count wild-seed)      => 1
     (author-count little-schemer) => 2)
 
-  (facts "12 multiple-authors?"
+  (facts "multiple-authors?" {:exercise 12
+                              :points 1}
          (multiple-authors? cities)         => false
          (multiple-authors? wild-seed)      => false
          (multiple-authors? little-schemer) => true)
   
-  (facts "13 add-author"
+  (facts "add-author" {:exercise 13
+                       :points 1}
     (add-author little-schemer {:name "Gerald J. Sussman"})
       => {:title "The Little Schemer"
           :authors [{:birth-year 1944, :name "Daniel Friedman"}
@@ -152,19 +166,23 @@
     (add-author {:authors [{:name "Juhana"}]} {:name "Jani"})
       => {:authors [{:name "Juhana"} {:name "Jani"}]})
   
-  (facts "14 alive?"
+  (facts "alive?" {:exercise 14
+                   :points 1}
     (alive? china)   => true
     (alive? octavia) => false)
   
-  (facts "15 element-lengths"
+  (facts "element-lengths" {:exercise 15
+                            :points 1}
     (element-lengths ["foo" "bar" "" "quux"])  => [3 3 0 4]
     (element-lengths ["x" [:a :b :c] {:y 42}]) => [1 3 1])
   
-  (facts "16 second-elements"
+  (facts "second-elements" {:exercise 16
+                            :points 1}
     (second-elements [[1 2] [2 3] [3 4]])               => [2 3 4]
     (second-elements [[1 2 3 4] [1] ["a" "s" "d" "f"]]) => [2 nil "s"])
   
-  (facts "17 titles"
+  (facts "titles" {:exercise 17
+                   :points 1}
     (titles [cities]) => ["The City and the City"]
     (titles books)    => (just ["The City and the City"
                                 "Wild Seed"
@@ -172,29 +190,34 @@
                                 "The Little Schemer"]
                                :in-any-order)))
 
-(facts "18 stars"
+(facts "stars" {:exercise 18
+                :points 1}
   (stars 1) => "*"
   (stars 7) => "*******"
   (stars 3) => "***")
 
-(facts "19 monotonic?"
+(facts "monotonic?" {:exercise 19
+                     :points 1}
        (monotonic? [1 2 3])     => true
        (monotonic? [0 1 10 11]) => true
        (monotonic? [3 2 0 -3])  => true
        (monotonic? [3 2 2])     => true
        (monotonic? [1 2 1 0])   => false)
 
-(facts "20 toggle"
+(facts "toggle" {:exercise 20
+                 :points 1}
   (toggle #{:a :b :c} :d) => #{:a :b :c :d}
   (toggle #{:a :b :c} :a) => #{:b :c})
 
-(facts "21 contains-duplicates?"
+(facts "contains-duplicates?" {:exercise 21
+                               :points 1}
   (contains-duplicates? [1 1 2 3 -40])   => true
   (contains-duplicates? [1 2 2 3 -40])   => true
   (contains-duplicates? [1 2 3 -40])     => false
   (contains-duplicates? [1 2 3 "a" "a"]) => true)
 
-(facts "22 old-book->new-book"
+(facts "old-book->new-book" {:exercise 22
+                             :points 1}
   (old-book->new-book {:title "The Little Schemer"
                        :authors [friedman, felleisen]})
     => {:title "The Little Schemer" :authors #{friedman, felleisen}}
@@ -236,31 +259,37 @@
 
       books [cities, wild-seed, embassytown, little-schemer]]
 
-  (facts "23 has-author?"
+  (facts "has-author?" {:exercise 23
+                        :points 1}
     (has-author? cities china)             => true
     (has-author? cities felleisen)         => false
     (has-author? little-schemer felleisen) => true
     (has-author? little-schemer friedman)  => true
     (has-author? little-schemer octavia)   => false)
 
-  (facts "24 authors"
+  (facts "authors" {:exercise 24
+                    :points 1}
     (authors [cities, wild-seed])              => #{china, octavia}
     (authors [cities, wild-seed, embassytown]) => #{china, octavia}
     (authors [little-schemer, cities])         => #{china, friedman, felleisen})
 
-  (facts "25 all-author-names"
+  (facts "all-author-names" {:exercise 25
+                             :points 1}
     (all-author-names []) => #{}
     (all-author-names [cities, wild-seed])
       => #{"China Miéville" "Octavia E. Butler"}
     (all-author-names books)
       => #{"Matthias Felleisen" "China Miéville"
            "Octavia E. Butler" "Daniel Friedman"})
-  (facts "26 author->string"
+
+  (facts "author->string" {:exercise 26
+                           :points 1}
     (author->string felleisen) => "Matthias Felleisen"
     (author->string friedman)  => "Daniel Friedman (1944 - )"
     (author->string octavia)   => "Octavia E. Butler (1947 - 2006)")
 
-  (facts "27 authors->string"
+  (facts "authors->string" {:exercise 27
+                            :points 1}
     (authors->string (:authors little-schemer))
          => (every-checker (contains "Daniel Friedman (1944 - )")
                            (contains "Matthias Felleisen")
@@ -272,43 +301,50 @@
                           (contains "Daniel Friedman (1944 - )")
                           (contains ", ")))
 
-  (facts "28 book->string"
+  (facts "book->string" {:exercise 28
+                         :points 1}
          (book->string wild-seed)
          => "Wild Seed, written by Octavia E. Butler (1947 - 2006)"
     (book->string little-schemer)
       => (every-checker (has-prefix "The Little Schemer, written by ")
                         (has-suffix #"Daniel Friedman \(1944 - \), Matthias Felleisen|Matthias Felleisen, Daniel Friedman \(1944 - \)")))
 
-  (facts "29 books->string"
+  (facts "books->string" {:exercise 29
+                          :points 1}
     (books->string []) => "No books."
     (books->string [cities])
       => "1 book. The City and the City, written by China Miéville (1972 - )."
     (books->string [little-schemer, cities, wild-seed])
       => #"3 books. The Little Schemer, written by (Daniel Friedman \(1944 - \), Matthias Felleisen|Matthias Felleisen, Daniel Friedman \(1944 - \)). The City and the City, written by China Miéville \(1972 - \). Wild Seed, written by Octavia E. Butler \(1947 - 2006\).")
 
-  (facts "30 books-by-author"
+  (facts "books-by-author" {:exercise 30
+                            :points 1}
     (books-by-author china books)   => (just [cities embassytown])
     (books-by-author octavia books) => (just [wild-seed]))
 
-  (facts "31 author-by-name"
+  (facts "author-by-name" {:exercise 31
+                           :points 1}
     (author-by-name "Octavia E. Butler" authors-set)            => octavia
     (author-by-name "Octavia E. Butler" #{felleisen, friedman}) => nil
     (author-by-name "China Miéville" authors-set)               => china
     (author-by-name "Goerge R. R. Martin" authors-set)          => nil)
 
-  (facts "32 living-authors"
+  (facts "living-authors" {:exercise 32
+                           :points 1}
     (living-authors authors-set)         => (just #{china, felleisen, friedman})
     (living-authors #{octavia})          => (just #{})
     (living-authors #{china, felleisen}) => (just #{china, felleisen}))
 
-  (facts "33 has-a-living-author?"
+  (facts "has-a-living-author?" {:exercise 33
+                                 :points 1}
     (has-a-living-author? wild-seed)      => false
     (has-a-living-author? silmarillion)   => true
     (has-a-living-author? little-schemer) => true
     (has-a-living-author? cities)         => true
     (has-a-living-author? deus-irae)      => false)
   
-  (facts "34 books-by-living-authors"
+  (facts "books-by-living-authors" {:exercise 34
+                                    :points 1}
     (books-by-living-authors books) => (just #{little-schemer cities embassytown})
     (books-by-living-authors (concat books [deus-irae, silmarillion]))
     => (just #{little-schemer cities embassytown silmarillion})))
