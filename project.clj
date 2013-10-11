63c4aa64143928ad923a04db20ac509c5620160b
Taxus <sami.lunnamo@cs.helsinki.fi>
2013-10-11 15:55:45 +0300

    whatthecommit

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject i-am-a-horse-in-the-land-of-booleans "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [midje-grader "0.1.0-SNAPSHOT"]]
   :profiles {:dev
              {:dependencies [[midje "1.5.1"]]
               :plugins [[lein-midje "3.0.0"]]}})
+
