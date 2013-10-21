71655bf1bc2574d320fe2289f0f074a4fdc2f9e2
Olli Koistinen <olko@cs.helsinki.fi>
2013-10-21 15:17:02 +0300

    Implemented boolean, abs, divides?, fizzbuzz, teen?, not-teen?, generic-doublificate and leap-year?

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,50 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (and
+       (not= x false)
+       (not= x nil))
+       true
+       false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (* -1 x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n)  "fizz"
+   (divides? 5 n)  "buzz"
+   :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (or
+    (list? x)
+    (vector? x)) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (if (= (mod year 4) 0)
+    (if (= (mod year 100) 0)
+      (if (= (mod year 400) 0)
+        true
+        false)
+      true)
+    false))
 
 ; '_______'
+
