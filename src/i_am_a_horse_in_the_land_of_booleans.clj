48e625fac1763abc1a2a603a4c6f347c767518ee
Mikko Översti <mikko.oversti@helsinki.fi>
2013-10-21 10:51:41 +0300

    lolol

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,19 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ( if x )
-    ( true )
-    ( false ))
+  ( if x 
+     true 
+     false ))
 
 (defn abs [x]
-  ( if x<0 )
-    ( -x )
-    ( x ))
+  ( if (< x 0) 
+     (- x) 
+     x ))
 
 (defn divides? [divisor n]
-  ( if ( == ( mod n divisor ) 0 ) )
-    ( true )
-    ( false ))
+  ( if ( == ( mod n divisor ) 0 ) 
+     true 
+     false ))
 
 (defn fizzbuzz [n]
   (cond
@@ -24,12 +24,12 @@
     :else ""))
 
 (defn teen? [age]
-  ( if ( <= 13 age 19 ) )
-    ( true )
-    ( false ))
+  ( if ( <= 13 age 19 ) 
+     true 
+     false ))
 
 (defn not-teen? [age]
-  not ( teen? age ))
+  (not ( teen? age )))
 
 (defn generic-doublificate [x]
   (cond
