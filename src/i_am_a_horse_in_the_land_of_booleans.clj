3f11cef078b481fd89682735c7e9fdd13cc338f4
Lauri I Kaitala <lauri.kaitala@helsinki.fi>
2013-10-21 14:19:54 +0300

    message here

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,33 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x
+    true
+    false))
 
 (defn abs [x]
-  ":(")
+  (if (< 0 x)
+    x
+    (* x -1)))
 
-(defn divides? [divisor n]
-  ":(")
+(defn
+  divides?
+  [divisor n]
+  (if (= 0
+         (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
   ":(")
 
-(defn teen? [age]
-  ":(")
+(defn
+  teen?
+  [age]
+  (if (<= 13 age)
+    (if (<= age 19)
+      true)
+    false))
 
 (defn not-teen? [age]
   ":(")
