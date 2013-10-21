fd6ca238a03e18748465056f8c773397805c5819
JRG <donjorgo@gmail.com>
2013-10-21 15:57:01 +0300

    prject.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject i-am-a-horse-in-the-land-of-booleans "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [midje-grader "0.1.0-SNAPSHOT"]]
   :profiles {:dev
              {:dependencies [[midje "1.5.1"]]
