0685bb3f2139e610f873201edb1c032f5247112b
Jan Ekström <jeebjp@gmail.com>
2013-10-24 16:22:22 +0300

    Funky test-succeeding code

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,39 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* x -1) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== 0 (mod n divisor)) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+     (== 0 (mod n 15)) "gotcha!"
+     (== 0 (mod n 3))  "fizz"
+     (== 0 (mod n 5))  "buzz"
+     :else             ""))
 
 (defn teen? [age]
-  ":(")
+  (if (or (< age 13) (> age 19)) false true))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+     (number? x) (* 2 x)
+     (empty?  x) nil
+     (or (list? x) (vector? x)) (* (count x) 2)
+     :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (if (= 0 (mod year 4))
+    (cond
+       (= 0 (mod year 400)) true
+       (= 0 (mod year 100)) false
+       :else                true) false))
 
 ; '_______'
