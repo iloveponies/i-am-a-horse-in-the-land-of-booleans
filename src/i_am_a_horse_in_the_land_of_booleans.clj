3e19a4bed20cbf780b45046dfa8031976f20af8f
Teemu Hurme <teemu.hurme@cs.helsinki.fi>
2013-10-24 15:22:36 +0300

    booleans

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,52 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if ( or (= x nil) (= x false))
+    false
+    true)
+  )
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+     x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0)
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (= (mod n 15) 0) "gotcha!"
+   (= (mod n 5) 0) "buzz"
+   (= (mod n 3) 0) "fizz"
+   :else ""
+   ))
 
 (defn teen? [age]
-  ":(")
+  (if (< 12 age 20)
+    true
+    false))
 
 (defn not-teen? [age]
-  ":(")
+  (if (teen? age)
+    false
+    true))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (= (mod year 400) 0) true
+   (= (mod year 100) 0) false
+   (= (mod year 4) 0) true
+   :else false
+   ))
 
 ; '_______'
