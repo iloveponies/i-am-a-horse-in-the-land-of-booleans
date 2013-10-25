7a81fc15e478fe37c077981ff9d789bd0c20cc76
Olavi Lintumäki <atomimalli@gmail.com>
2013-10-25 15:37:48 +0300

    excercises done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,75 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if
+    (= x nil)
+    false
+    (if
+      (= x false)
+      false
+      true
+      )
+    )
+  )
 
 (defn abs [x]
-  ":(")
+  (if
+    (> x 0)
+    x
+    (- x)
+    )
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0
+      (mod n divisor)
+      )
+  )
+
+
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+
+   :else ""
+   )
+  )
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+  )
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true
+   )
+  )
 
 (defn leap-year? [year]
-  ":(")
+  (if
+    (divides? 4 year)
+    (if
+      (divides? 100 year)
+      (if
+        (divides? 400 year)
+        true
+        false
+        )
+      true
+      )
+    false
+    )
+  )
 
 ; '_______'
+
+
