fc9842b9d38bc29ab70aacdadfc5ba1328b40f5c
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-06 21:47:42 +0200

    travis test

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,35 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  -x)
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (zero? (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (divides? 15 n) "gotcha!"
+        (divides? 5 n) "buzz"
+        (divides? 3 n) "fizz"
+        :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* 2 x)
+        (and (coll? x) (empty? x)) nil
+        (or (list? x) (vector? x)) (* 2 (count x))
+        :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (or (divides? 400 year)
+      (and (divides? 4 year)
+           (not (divides? 100 year)))))
 
 ; '_______'
