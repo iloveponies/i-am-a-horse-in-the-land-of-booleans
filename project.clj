0ab503e2d0bc0d4138cd9a7bf547a127e306d720
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:08:50 +0200

    Tests as a separate project

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject structured-data "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje-grader "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.1.1"]]}})
+                 [iloveponies.tests/structured-data "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
 
