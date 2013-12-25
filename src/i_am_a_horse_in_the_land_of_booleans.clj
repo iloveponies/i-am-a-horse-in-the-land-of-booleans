f6839651111033f2d140fe55a33f15b6ce5fd141
Miko Mynttinen <miko.mynttinen@helsinki.fi>
2013-12-25 21:32:53 +0200

    finished with horses

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,54 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (= x nil) false
+    (if (= x false) false true)
+    )
+  )
 
 (defn abs [x]
-  ":(")
+ (if (< x 0) (- x) (+ x))
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0)
+  )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   :else "")
+  )
+
 
 (defn teen? [age]
-  ":(")
+  (if (> age 12)
+    (if (< age 20) true false)
+  false)
+  )
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+  )
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true)
+  )
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (and (divides? 400 year)
+        (divides? 100 year)) true
+   (and (divides? 4 year)
+        (divides? 100 year)) false
+   (divides? 4 year) true
+   :else false)
+  )
 
 ; '_______'
