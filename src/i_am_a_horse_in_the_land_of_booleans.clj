aba5672c1b79f99e5aafac8319d1ea2c1611ae2b
Adam Rulli-Gibbs <git@status203.com>
2014-03-04 17:11:33 +0000

    all tests pass?

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,38 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or (= x false) (= x nil)) false true))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (zero? (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (divides? 15 n) "gotcha!"
+        (divides? 5 n)  "buzz"
+        (divides? 3 n)  "fizz"
+        :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* 2 x)
+        (empty? x) nil
+        (or (list? x) (vector? x)) (* 2 (count x))
+        :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond (divides? 400 year) true
+        (divides? 100 year) false
+        (divides? 4 year) true
+        :else false))
 
 ; '_______'
