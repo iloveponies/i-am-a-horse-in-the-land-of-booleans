1695a5330347f1fd48160356cf3fc611d6980306
James Chochlinski <jchochli@xpzen.com>
2014-03-07 16:25:40 -0500

    learnin'

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -15,9 +15,9 @@
 
 (defn fizzbuzz [n]
   (cond
-   (divides? 3 n) "fizz"
-   (divides? 5 n) "buzz"
    (divides? 15 n) "gotcha!"
+   (divides? 5 n) "buzz"
+   (divides? 3 n) "fizz"
    :else ""))
 
 (defn teen? [age]
@@ -26,12 +26,21 @@
     false))
 
 (defn not-teen? [age]
-  (if (teen? age) false true))
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (list? x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (divides? 400 year) true
+   (and (divides? 4 year) (not (divides? 100 year))) true
+   :else false
+   ))
 
 ; '_______'
