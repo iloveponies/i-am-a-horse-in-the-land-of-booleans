6c6705a5b2327effd93ab843036f6850bb146b91
James Chochlinski <jchochli@xpzen.com>
2014-03-07 14:51:53 -0500

    partial

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,22 +2,31 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if
+      (== (mod n divisor) 0)
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   (divides? 15 n) "gotcha!"
+   :else ""))
 
 (defn teen? [age]
-  ":(")
+  (if (< age 20)
+    (if (>= age 13) true false)
+    false))
 
 (defn not-teen? [age]
-  ":(")
+  (if (teen? age) false true))
 
 (defn generic-doublificate [x]
   ":(")
