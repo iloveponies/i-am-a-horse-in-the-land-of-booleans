21c3d7eaf5b2156132600e85749c203a3b2c2196
guillaume-thouvenin <guillaume.thouvenin@bull.net>
2014-03-14 21:11:54 +0100

    Resolved problems

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,69 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (= x nil)
+    false
+    (if (= x false)
+      false
+      true
+      )
+    )
+  )
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x
+    )
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0)
+    true
+    false
+  )
+  )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""
+    )
+  )
 
 (defn teen? [age]
-  ":(")
+  (if (< age 13)
+    false
+    (if (> age 19)
+      false
+      true
+     )
+    )
+  )
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+  )
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else true
+    )
+  )
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false
+    )
+  )
 
 ; '_______'
