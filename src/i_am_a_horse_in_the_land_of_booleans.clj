4b2fb96d12e45bb956a955860afcc2517e352eb9
firehooper <sam>
2014-05-30 23:40:44 -0400

    completed booleans

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,42 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3  n) "fizz"
+   (divides? 5  n) "buzz"
+   :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* (count x) 2)
+   :else true))
+
 
 (defn leap-year? [year]
-  ":(")
+  (defn mod-year [x]
+    (zero? (mod year x)))
+  (cond
+   (mod-year 400) true
+   (mod-year 100) false
+   (mod-year 4) true
+   :else false))
 
-; '_______'
+                                        ; '_______'
