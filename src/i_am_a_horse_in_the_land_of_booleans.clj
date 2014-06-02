3460bb3e85726abd07c02189e47c4a8504def267
Huomo Tero A <tahuomo@da3-cs-dk110-08.helsinki.fi>
2014-06-02 16:43:58 +0300

    second set done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,42 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+     (- x)
+     x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== 0 (mod n divisor))
+      true
+      false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (== 0 (mod n 15)) "gotcha!"
+   (== 0 (mod n 3)) "fizz"
+   (== 0 (mod n 5)) "buzz"
+   :else            ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (or (> 13 age) (< 19 age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x)  nil
+   (or (list? x) (vector? x))   (* 2 (count x))
+   :else       true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (== 0 (mod year 400)) true
+   (and (== 0 (mod year 4)) (not (== 0 (mod year 100)))) true
+   :else       false))
 
 ; '_______'
