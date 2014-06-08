b05acab6ca548e852edce2815f0f921a6c61c147
Sergey Vasiliev <brighterr@gmail.com>
2014-06-09 01:36:43 +0400

    In progress

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,23 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (neg? x) (* -1 x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    (divides? 15 n) "gotcha!"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
   ":(")
