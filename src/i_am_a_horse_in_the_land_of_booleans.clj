b8d27f9ad0f131b25225524efa9409769bf3d46f
Sergey Vasiliev <nblpblc@gmail.com>
2014-06-13 22:17:37 +0400

    Provide I am a horse in the land of booleans Solutions

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -12,21 +12,28 @@
 
 (defn fizzbuzz [n]
   (cond
-    (divides? 3 n) "fizz"
-    (divides? 5 n) "buzz"
     (divides? 15 n) "gotcha!"
+    (divides? 5 n) "buzz"
+    (divides? 3 n) "fizz"
     :else ""))
 
 (defn teen? [age]
   (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (vector? x) (* 2 (count x))
+    (list? x) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (or (and (divides? 4 year)
+           (not(divides? 100 year)))
+      (divides? 400 year)))
 
 ; '_______'
