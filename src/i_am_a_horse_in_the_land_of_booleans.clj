d3f1687eea6dac98a9a0689d3f56bd12c0177503
Patrick Gallagher <paddy.gallagher@gmail.com>
2014-06-29 15:19:28 +0100

    Solutions to Exercises 2, 3 and 4

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -5,16 +5,16 @@
   (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
   ":(")
 
 (defn teen? [age]
-  ":(")
+  (< 13 age 19))
 
 (defn not-teen? [age]
   ":(")
