2755aa42b838054f6fbd9b4fbad5deb9a97a9141
Jonas Pommerening <jonas.pommerening@gmail.com>
2014-06-29 23:12:02 +0200

    Exercise 8: leap-year?

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -33,6 +33,12 @@
     ))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false
+    )
+  )
 
 ; '_______'
