b92398895052319393349e3f61947520c922e212
guy <guybarlow@powa.com>
2014-07-03 15:58:20 +0100

    exercise 8 was fun

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,64 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean
+  [x]
+  (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn abs
+  [x]
+  (if (< x 0) (* -1 x) x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides?
+  [div n]
+  (= (mod n div) 0))
+
+
+(defn leap-year?
+  [year]
+  (or
+   (divides? 400 year)
+   (and (divides? 4 year)  (not (divides? 100 year)))))
 
 (defn fizzbuzz [n]
-  ":(")
+   [n]
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen?
+  [age]
+  (and (>= age 13) (<= age 19)))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen?
+  [age]
+  (not (teen? age)))
 
-(defn generic-doublificate [x]
-  ":(")
-
-(defn leap-year? [year]
-  ":(")
 
 ; '_______'
+
+
+(defn sign
+  [x]
+  (if (< x 0) "-" "+"))
+
+
+(defn super-sign
+  [number]
+  (cond
+   (neg? number) "negative"
+   (pos? number) "postive"
+   :else         "zero" ))
+
+
+
+(defn generic-doublificate
+  [x]
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (list? x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   :else true))
