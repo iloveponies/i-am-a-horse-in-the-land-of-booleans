69a4f48f06eeff660983c65c5d8944eccbebf411
Dmitry Mylnev <Dmitry.Mylnev@traderstraffic.com>
2014-07-04 18:40:31 +0400

    Finished Exercise 1

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,40 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
+
+;;for nil and false, it returns false, and for all other values it returns true
+
 (defn boolean [x]
-  ":(")
+  (if (or (= x nil) (= x false))
+    false true))
+
 
 (defn abs [x]
   ":(")
 
+
 (defn divides? [divisor n]
   ":(")
+
 
 (defn fizzbuzz [n]
   ":(")
 
+
 (defn teen? [age]
   ":(")
+
 
 (defn not-teen? [age]
   ":(")
 
+
 (defn generic-doublificate [x]
   ":(")
 
+
 (defn leap-year? [year]
   ":(")
+
 
 ; '_______'
