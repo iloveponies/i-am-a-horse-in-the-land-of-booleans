a3812c1230a50dd079ba7902a4373e4d9349a426
Andy <ANDY@andys-macbook.(none)>
2014-08-07 15:30:20 -0600

    Partial results for boolean module

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,31 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or
+       (false? x)
+       (nil? x))
+    false
+    true))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (* -1 x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0)
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 5 n)  "buzz"
+   (divides? 3 n)  "fizz"
+   :else            ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
   ":(")
