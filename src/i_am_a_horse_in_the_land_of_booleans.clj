2688ebc6b0c06afb161ab6c50a5bcfdce9281f8d
David Corney <david.corney.1@city.ac.uk>
2014-08-11 18:39:39 +0100

    DC's submission

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,57 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x)
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor)) true false)
+
+  )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   :else ""
+   )
+  )
 
 (defn teen? [age]
-  ":(")
+  (if (>= age 13) true false)
+  )
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+  )
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (list? x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   :else true
+   )
+  )
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (divides? 400 year) true
+   (divides? 100 year) false
+   (divides? 4 year) true
+   :else false
+   )
+  )
 
-; '_______'
+(defn boolean [x]
+
+  (if (= x false) (false)
+    (if (= x nil) false
+      (true)))
+)
+
+;'_______'
