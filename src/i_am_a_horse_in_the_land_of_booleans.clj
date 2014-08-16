a6f77b177a59906abb496eca8039cd2173c9ecbd
Alex Podaras <apodaras@gmail.com>
2014-08-16 13:25:26 +0300

    Solve exercise 5

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -11,7 +11,11 @@
   (zero? (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 5 n)  "buzz"
+   (divides? 3 n)  "fizz"
+   :else           ""))
 
 (defn teen? [age]
   (<= 13 age 19))
