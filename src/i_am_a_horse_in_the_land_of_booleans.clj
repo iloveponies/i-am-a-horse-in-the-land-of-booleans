d1fbdc2f090a77bebbd7f7523ba142a9317caf38
vladimirjeune <randyjeune@gmail.com>
2014-09-19 01:18:55 -0700

    Do half of the chapter

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -8,17 +8,36 @@
   ( not (not x) )  ; Makes sure we are only comparing boolean values, so only bools returned
   ))
 
-(defn abs [x]
-  ":(")
+(defn
+  abs
+  "Returns the absolute value of x"
+  [x]
+  (if (< x 0)
+    (* -1 x)
+    x )
+  )
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides?
+  "Returns true if divisor divides n, false otherwise"
+  [divisor n]                         ; This is passing in 2 params
+   (if ( == 0 (mod n divisor) )
+     true
+     false) )
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz
+  "Returns 'fuzz' when /3, 'buzz' when /5 'gotcha' /15 and '' otherwise"
+  [n]
+  (cond
+   (divides? 15 n) "gotcha!"           ; This is like an elseif, not ifs.
+   (divides? 5  n) "buzz"
+   (divides? 3  n) "fizz"
+   :else           ""  ) )
 
-(defn teen? [age]
-  ":(")
+(defn
+  teen?
+  "Return truthy if 19 >= age >= 13"
+  [age]
+  ( >= 19 age 13 ) )
 
 (defn not-teen? [age]
   ":(")
