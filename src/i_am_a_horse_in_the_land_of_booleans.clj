875df78d9989ee7f206f9b7dc91e2ac5bd0049cc
unknown <pravinkumary@PravinkumarY-MS.mumbai1.corp.citiustech.com>
2014-10-07 13:26:43 +0530

    Completed

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,37 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x](if x true false))
 
-(defn abs [x]
-  ":(")
+(defn abs [x] (if(< x 0)(- x) x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? [divisor n] (if(zero? (mod n divisor)) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n)"gotcha!"
+    (divides? 5 n)"buzz"
+    (divides? 3 n)"fizz"
+     :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen? [age](if (>= 19 age 13) true false))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? [age](if(not (teen? age)) true false))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x)(* x 2)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true))
 
-(defn leap-year? [year]
-  ":(")
+;(defn leap-year? [year] (or
+                        ; (and (divides? 4 year) (not (divides? 100 year)))
+                         ;(and (divides? 100 year) (divides? 400 year))))
+
+  (defn leap-year? [year] (and
+                           (divides? 4 year)
+                           (or (not (divides? 100 year)) (divides? 400 year) )
+                           ))
 
 ; '_______'
