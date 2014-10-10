5c7d215a301e827e2eb10fff411a5dd70a755123
Eng <engd@sea-engd-m.ds.ad.adp.com>
2014-10-10 14:33:38 -0700

    submit tests passing

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,40 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false)
+  )
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* -1 x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+ (cond 
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   :else ""))
 
 (defn teen? [age]
-  ":(")
+  (and (< age 20) (> age 12)))
 
 (defn not-teen? [age]
-  ":(")
+ (not (teen? age))) 
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond 
+    (number? x) (* x 2)
+    (coll? x) (cond 
+                (empty? x) nil
+                (or (list? x) (vector? x)) (* 2 (count x))
+                :else true)
+     :else true
+      ))
 
 (defn leap-year? [year]
-  ":(")
+  (or (and (not (< 0 (mod year 4))) (not (= 0 (mod year 100)))) (not (< 0 (mod year 400))))
+  )
 
 ; '_______'
