cdd173bbd722acb424c18aefb0ae06a06f14d824
Matthew M Nelson <dogenpunk@gmail.com>
2014-10-14 22:14:39 -0500

    All tests passing

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,45 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or (nil? x) (false? x))
+    false
+    true))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (= 0 (mod n 15)) "gotcha!"
+    (= 0 (mod n 5))  "buzz"
+    (= 0 (mod n 3))  "fizz"
+    :else            ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x)  nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (not (= 0 (mod year 4)))   false
+    (not (= 0 (mod year 100))) true
+    (not (= 0 (mod year 400))) false
+    :else                      true))
 
 ; '_______'
