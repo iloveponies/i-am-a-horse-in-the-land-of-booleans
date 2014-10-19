d5b9c744414fbcb1beec96dbf0436670d22a5499
Sandra Luhtaniemi <anni.luhtaniemi@helsinki.fi>
2014-10-19 21:40:49 +0300

    tokat tehtävät tehty

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,54 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* -1 x) x))
 
 (defn divides? [divisor n]
-  ":(")
+   (= 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (= (mod n 15) 0) "gotcha!"
+   (= (mod n 3) 0) "fizz"
+   (= (mod n 5) 0) "buzz"
+   :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (or (> age 19) (> 13 age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (list? x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (or
+   (= 0 (mod year 4) (mod year 100) (mod year 400))
+   (and (= 0 (mod year 4)) (not(= 0 (mod year 100))))
+   ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ; '_______'
