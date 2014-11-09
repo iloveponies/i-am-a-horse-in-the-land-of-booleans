af25825e48e6e1d74deb8c02138fea038abd3134
Tomi Kasurinen <tomikasu@cs.helsinki.fi>
2014-11-09 13:40:27 +0200

    boolean pony

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,50 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n)  "gotcha!"
+   (divides? 5 n)   "buzz"
+   (divides? 3 n)   "fizz"
+   :else            ""
+   )
+  )
 
 (defn teen? [age]
-  ":(")
+  (if (< 12 age 20) true false))
 
 (defn not-teen? [age]
-  ":(")
+  (if (not (teen? age)) true false))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (list? x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   :else true
+   )
+  )
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (not (integer? (/ year 4))) false
+   (not (integer? (/ year 100))) true
+   (not (integer? (/ year 400))) false
+   :else true
+   )
+  )
 
 ; '_______'
