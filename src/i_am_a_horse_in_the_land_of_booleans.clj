46e61ec63997dc4e4cb5fb3f51973a1be30b31d3
jp <jp@hekovnik.si>
2014-11-11 14:46:03 +0100

    Exercise completion

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,52 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn
+  boolean
+  "Takes one parameter and returns true for everything but nil and false values."
+  [x]
+  (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn
+  abs
+  "takes one parameter and returns absolute value of that parameter"
+  [x]
+  (if (< x 0)
+    (* -1 x)
+    x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn
+  divides?
+  "Takes two parameters divisor and n and returns true if divisor devides n and false othervise."
+  [divisor n]
+  (if (= 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 5 n) "buzz"
+   (divides? 3 n) "fizz"
+   :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn
+  teen?
+  "takes one parameter age and returns true if age is at least 13 and at most 19"
+  [age]
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (and (divides? 4 year) (or (not (divides? 100 year)) (divides? 400 year))))
 
 ; '_______'
