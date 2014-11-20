2cd5cbca2050473f4722086ad2df2896524e5ea3
Gopal Ramachandran <gsebastianr@gmail.com>
2014-11-20 16:57:01 -0500

    second pset, first attempt

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,41 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- 0 x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (= (mod n 15) 0) "gotcha!"
+   (= (mod n 3 ) 0) "fizz"
+   (= (mod n 5 ) 0) "buzz"
+   :else            ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty?  x)  nil
+   (list?   x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   :else true))
+
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (= (mod year 400) 0) true
+   (= (mod year 100) 0) false
+   (= (mod year   4) 0) true
+   :else              false))
 
 ; '_______'
