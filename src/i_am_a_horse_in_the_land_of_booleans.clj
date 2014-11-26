519bbf44dce8403f2acd145be98276dbabb8cc54
Olli Björkqvist <olof@crunchbang.(none)>
2014-11-26 15:34:07 +0200

    test ok

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,27 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (= 0 (mod n 15)) "gotcha!" (= 0 (mod n 5)) "buzz" (= 0 (mod n 3)) "fizz" :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* 2 x) (empty? x) nil (list? x) (* 2 (count x)) (vector? x) (* 2 (count x)) :else true))
 
 (defn leap-year? [year]
-  ":(")
+ (or (= 0 (mod year 400)) (and (not (= 0 (mod year 100))) (= 0 (mod year 4)))))
 
 ; '_______'
