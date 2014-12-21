98c5407ab335b1de11ac7d32d8f08e6a0a5b108d
hindenbug <manoj.mk27@gmail.com>
2014-12-21 23:00:18 +0530

    condtions

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,25 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0)
+    true
+    false))
 
 (defn fizzbuzz [n]
   ":(")
 
 (defn teen? [age]
-  ":(")
+  (if (<= 13 age 19)
+    true
+    false))
 
 (defn not-teen? [age]
   ":(")
