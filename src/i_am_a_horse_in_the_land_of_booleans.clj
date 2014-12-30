80507f22893af7ec1e726824d051cac60db01110
Ken Are Astrup <ken.astrup@gmail.com>
2014-12-30 14:24:02 +0100

    generic-doublificate + fikser

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -16,9 +16,9 @@
 
 (defn fizzbuzz [n]
   (cond
+    (divides? 15 n) "gotcha!"
     (divides? 3 n) "fizz"
     (divides? 5 n) "buzz"
-    (divides? 15 n) "gothca!"
     :else ""))
 
 (defn teen? [age]
@@ -28,7 +28,13 @@
   ":(")
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x ) (+ x x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x)  (* 2 (count x))
+    :else true
+    ))
 
 (defn leap-year? [year]
   ":(")
