ed3a3be0f4566e689b9a0b0c2d5b55e533de4ee8
Isaac Boehman <isaac@bitsighttech.com>
2015-01-02 18:20:31 -0500

    completes exercise 5

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -11,7 +11,11 @@
   (== (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 5 n) "buzz"
+    (divides? 3 n) "fizz"
+    :else          ""))
 
 (defn teen? [age]
   (<= 13 age 19))
