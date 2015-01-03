fe3d366dc70b017be582264d3631aff0e772f0c7
Ben Marx <benyamin.marx@gmail.com>
2015-01-03 22:47:15 +0900

    almost finished

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,50 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (cond
+   (= x nil) false
+   (= x false) false
+   :else true))
+
 
 (defn abs [x]
-  ":(")
+  (if (> x 0)
+      x
+      (* -1 x)))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3  n) "fizz"
+   (divides? 5  n) "buzz"
+   :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+)
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (not (divides? 4 year)) false
+   (not (divides? 100 year)) true
+   (not (divides? 400 year)) false
+   :else true))
 
+
+;if (year is not divisible by 4) then (it is a common year)
+;else
+;if (year is not divisible by 100) then (it is a leap year)
+;else
+;if (year is not divisible by 400) then (it is a common yeaar)
+;else (it is a leap year)
 ; '_______'
