386a9e895538c93215d657632275190b1bd3c77d
Teemu Heikkilä <teemu@swan>
2015-01-03 20:51:13 +0200

    Horrrssee ready

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,56 @@
-(ns i-am-a-horse-in-the-land-of-booleans
+                                                                                                                                                                                                                                                              (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false)
+  )
 
 (defn abs [x]
-  ":(")
+  (if (> x 0) x (* x -1))
+)
+
 
 (defn divides? [divisor n]
-  ":(")
+  (= 0 (mod n divisor))
+  )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (divides? 15 n) "gotcha!"
+        (divides? 3 n) "fizz"
+        (divides? 5 n) "buzz"
+        :else ""
+  )
+)
+
 
 (defn teen? [age]
-  ":(")
+  (if
+    (and
+     (>= age 13)
+     (<= age 19)
+     )
+    true
+    false
+    )
+  )
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+)
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true
+  )
+)
 
 (defn leap-year? [year]
-  ":(")
-
+  (if (and (divides? 4 year) (not (divides? 100 year)))
+    true
+    (divides? 400 year)
+  )
+)
 ; '_______'
