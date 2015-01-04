2062bf076f634ce7f34bbb6f410538ee407babe3
lszydlo <szydler@gmail.com>
2015-01-04 23:26:40 +0100

    implement leap year

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -36,7 +36,11 @@
     :else               true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (zero? (mod year 400)) true
+    (zero? (mod year 100)) false
+    (zero? (mod year 4))   true
+    :else                  false))
 
 ;(i-am-a-horse-in-the-land-of-booleans/abs 123)
 ; '_______'
