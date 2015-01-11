6f8aeeb9c0fe5be4dd7c1384d858e185f8aef533
Martin Karlsson <tankrim@gmail.com>
2015-01-11 21:03:51 +0100

    Part 2 done(?).

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,47 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x 'true 'false))
 
 (defn abs [x]
-  ":(")
+  (if (<= 0 x)
+    x
+    (- x)))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (== 0 (mod n 15)) "gotcha!"
+        (== 0 (mod n 3)) "fizz"
+        (== 0 (mod n 5)) "buzz"
+        :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (if (teen? age)
+    false
+    true))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* 2 x)
+        (empty? x) nil
+        (list? x) (* 2 (count x))
+        (vector? x) (* 2 (count x))
+        :else true))
 
-(defn leap-year? [year]
-  ":(")
+(defn leap-year?
+  "A year is a leap year if it is divisible by 4, except if it is
+  divisible by 100, in which case it still is a leap year if it is
+  divisible by 400."
+  [year]
+  (cond (not (== 0 (mod year 4))) false
+        (not (== 0 (mod year 100))) true
+        (not (== 0 (mod year 400))) false
+        :else true))
 
 ; '_______'
