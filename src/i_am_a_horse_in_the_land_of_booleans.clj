c845e4329f81ea8f8cc7bf9184128930c994209d
Simon Koelsch <simon.koelsch@innoq.com>
2015-01-16 23:21:46 +0100

    boar, pitstop

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,25 +2,37 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* x -1) x)
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (if (mod divisor n) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (== (mod n 15) 0) "gotcha!"
+   (== (mod n 5) 0) "buzz"
+   (== (mod n 3) 0) "fizz"
+   :else "")
+  )
 
 (defn teen? [age]
-  ":(")
+  (if (<= 13 age 19) true false)
+  )
 
 (defn not-teen? [age]
   ":(")
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x x)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true)
+  )
 
 (defn leap-year? [year]
   ":(")
