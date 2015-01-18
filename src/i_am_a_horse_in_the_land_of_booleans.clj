9f39993f2f65551a7363b3e71beb06608ce065fb
softwareresearchwork <softwareresearchwork@users.noreply.github.com>
2015-01-19 02:26:11 +0530

    Complete solutions for conditionals and boolean functions.

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,44 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x 
+    true
+    false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (generic-doublificate (count x))
+    (vector? x) (generic-doublificate (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (not (divides? 4 year)) false
+    (not (divides? 100 year)) true
+    (not (divides? 400 year)) false
+    :else true))
 
 ; '_______'
