f72ccd78092c287264c52043d398571636150e8f
Susanna Mäkinen <susannama@cashew.local>
2015-01-22 15:25:48 +0200

    tehtava2

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,90 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+
 
 (defn abs [x]
-  ":(")
-
-(defn divides? [divisor n]
-  ":(")
-
-(defn fizzbuzz [n]
-  ":(")
+  (if (< x 0)
+  (- x)
+  x
+    )
+  )
 
 (defn teen? [age]
-  ":(")
+  (if (< 12 age 20)
+    true
+    false
+  )
+  )
+
+(defn boolean [x]
+
+  (if (nil? x)
+   false
+    (if (= false x)
+      false
+      true
+    )
+    )
+)
+
+
+(defn divides? [divisor n]
+  (if (or (nil? n) )
+    false
+
+    (if (== 0 (mod n divisor))
+     true
+     false
+ )
+    )
+)
+
+(defn fizzbuzz [n]
+  (if (divides? 15 n)
+    "gotcha!"
+    ( if(divides? 3 n)
+      "fizz"
+      (if (divides? 5 n)
+        "buzz"
+        ""
+      )
+  )
+  )
+)
+
 
 (defn not-teen? [age]
-  ":(")
+  (if (or (< age 13) (> age 19))
+    true
+    false
+    )
+  )
 
 (defn generic-doublificate [x]
-  ":(")
+  (if (number? x)
+    (+ x x)
+    (if (empty? x)
+      nil
+      (if (or (list? x) (vector? x))
+        (* 2 (count x))
+        true
+        )
+      )
+  )
+)
 
 (defn leap-year? [year]
-  ":(")
+ ( if (divides? 400 year)
+   true
+   (if (divides? 100 year)
+     false
+     (if (divides? 4 year)
+       true
+       false
+       )
+     )
+   )
+  )
 
 ; '_______'
