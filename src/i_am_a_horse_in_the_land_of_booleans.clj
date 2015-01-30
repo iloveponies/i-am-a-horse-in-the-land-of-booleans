ba5b72c716e3e9e169ba5dab5f9293b280d33348
perrymann <perttu.nikander@cs.helsinki.fi>
2015-01-30 13:29:09 +0200

    palautus

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,36 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x] (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn abs [x] (if (< x 0) (* x -1) x))
 
-(defn divides? [divisor n]
-  ":(")
 
-(defn fizzbuzz [n]
-  ":(")
+(defn divides? [divisor n] (if (== (mod n divisor) 0) true false))
 
-(defn teen? [age]
-  ":(")
 
-(defn not-teen? [age]
-  ":(")
+(defn fizzbuzz [n] (cond
+                    (divides? 15 n) "gotcha!"
+                    (divides? 3 n) "fizz"
+                    (divides? 5 n) "buzz"
+                    :else ""))
 
-(defn generic-doublificate [x]
-  ":(")
 
-(defn leap-year? [year]
-  ":(")
+(defn teen? [age] (if (< 12 age 20) true false))
 
+
+(defn not-teen? [age] (if (not (teen? age)) true false))
+
+
+(defn generic-doublificate [x] (cond
+                                (number? x) (* 2 x)
+                                (empty? x) nil
+                                (list? x) (* 2 (count x))
+                                (vector? x)(* 2 (count x))
+                                :else true))
+(defn leap-year? [year] (cond
+                         (zero? (mod year 400)) true
+                         (zero? (mod year 100)) false
+                         (zero? (mod year 4)) true
+                         :else false))
 ; '_______'
