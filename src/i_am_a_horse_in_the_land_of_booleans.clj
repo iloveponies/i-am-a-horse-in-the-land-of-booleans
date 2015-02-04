dbfeb29a8877e9e4399017a60071d79bebb21a8e
Jukka-Pekka Seppänen <Jukkis@Jukka-Pekka-MacBook-Pro.local>
2015-02-04 21:33:20 +0200

    palautus

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,39 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0 ) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+     (== (mod n 15) 0) "gotcha!"
+     (== (mod n 3) 0) "fizz"
+     (== (mod n 5) 0) "buzz"
+     :else "" ))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+     (number? x) (* x 2)
+     (empty? x) nil
+     (list? x) (* (count x) 2)
+     (vector? x) (* (count x) 2)
+     :else true ))
 
 (defn leap-year? [year]
-  ":(")
+  (if (divides? 100 year)
+      (divides? 400 year)
+      (divides? 4 year)
+   ))
 
 ; '_______'
