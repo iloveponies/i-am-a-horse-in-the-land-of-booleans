bfe1fe581bf2ba578e650320e1a0e612a8102141
satukark <Satu@82-181-230-236.bb.dnainternet.fi>
2015-02-19 23:01:42 +0200

    Horse in the land of booleans
    Exercise 1-8

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,26 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x] (not (or ( = false x) ( = nil x) )))
 
-(defn abs [x]
-  ":(")
+(defn abs [x] (max x (- x)))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? [divisor n] (== 0 (mod n divisor )))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [n] (cond (divides? 15 n) "gotcha!" (divides? 5 n) "buzz" (divides? 3 n) "fizz" :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen? [age] (<= 13 age 19))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? [age](not (<= 13 age 19)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+  (number? x) (* 2 x)
+  (empty? x) nil
+  (or (vector? x) (list? x)) (* 2 (count x))
+  :else true))
 
-(defn leap-year? [year]
-  ":(")
+(defn leap-year? [year] (cond (== 0 (mod year 400)) true (== 0 (mod year 100)) false (== 0 (mod year 4)) true :else false))
+
 
 ; '_______'
