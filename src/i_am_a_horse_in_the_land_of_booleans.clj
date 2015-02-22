bdeac84e28f8342a4d13b0ffbc5bc198272a97d3
Gavin Coombes <gav@darkseas.org>
2015-02-22 20:20:47 +0800

    Up to exercise 4

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,40 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (not (contains? #{false nil} x)))
 
-(defn abs [x]
-  ":(")
+(defn abs [y] (if (< y 0)
+                (- y)
+                y))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
   ":(")
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  ";<")
 
-(defn generic-doublificate [x]
+ (defn generic-doublificate [x]
   ":(")
 
 (defn leap-year? [year]
   ":(")
 
 ; '_______'
+
+
+
+
+
+
+
+
+
+
+
+
