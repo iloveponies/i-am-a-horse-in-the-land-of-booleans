c52a6a28fd09d72622720fc3bfead7e2b7080161
Sahil Bajaj <sahil29@gmail.com>
2015-02-25 21:37:32 +0800

    Exercises 3 and 4

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -5,10 +5,14 @@
   (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
   ":(")
