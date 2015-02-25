2e2bff147d76d79cdb79b4007bf7427bb642c675
asena01 <dawit.weldemeskel@gmail.com>
2015-02-26 01:47:52 +0200

    secondExercise

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,44 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+    (if(not= x nil)
+    (if(not= x false)
+    (if(= x "foo") true (if(= x (+ 2 3))
+    true (if(= x true) true false))) false)
+    false)
+  )
 
 (defn abs [x]
-  ":(")
+    (if(< x 0) (* x -1) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if(= 0 (mod n divisor)) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+   (cond
+       (= 0 (mod n 15)) "gotcha!"
+       (= 0 (mod n 5)) "buzz"
+       (= 0 (mod n 3)) "fizz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+   (if(>= age 13)
+    (if(<= age 19) true false) false))
 
 (defn not-teen? [age]
-  ":(")
+      (or (< age 13) (> age 19)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+        (number? x) (+ x x)
+        (empty? x) nil
+        (vector? x) (+ (count x) (count x))
+        (list? x) (+ (count x) (count x))
+  :else true))
 
 (defn leap-year? [year]
-  ":(")
+     (or  (and (= 0 (mod year 4))
+     (not= 0 (mod year 100)))
+     (= 0 (mod year 400))))
 
 ; '_______'
