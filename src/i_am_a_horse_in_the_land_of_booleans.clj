c8eebad2cf51fee4d5881d89ff5971d70b99e1cb
Sahil Bajaj <sahil29@gmail.com>
2015-02-25 21:53:00 +0800

    Exercise 8: Leap year

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -35,6 +35,10 @@
    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (divides? 400 year) true
+   (divides? 100 year) false
+   (divides? 4 year) true
+   :else false))
 
 ; '_______'
