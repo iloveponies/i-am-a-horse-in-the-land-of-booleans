2515393d8611f5887f285fe6385287ba6d5ed9cf
Lenthanil <fredpolar@gmail.com>
2015-03-16 11:45:41 +0100

    Done. Remeber to at first fork and get the link to you private github forklein midje!

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,43 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    (+ x)))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (>= n divisor)
+    (zero? (mod n divisor))
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (zero? (mod n 15)) "gotcha!"
+    (zero? (mod n 5)) "buzz"
+    (zero? (mod n 3)) "fizz"
+    :else   ""))
 
 (defn teen? [age]
-  ":(")
+  (if (<= 13 age 19) true false))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+    (cond
+    (zero? (mod year 400)) true
+    (zero? (mod year 100)) false
+    (zero? (mod year 4)) true
+    :else  false))
 
 ; '_______'
