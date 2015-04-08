f7b321386314d03b1ac9327e54fc268e256ed415
Jieselle Urquhart <jurquhar@thoughtworks.com>
2015-04-08 12:31:25 +0100

    2nd try

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -4,28 +4,44 @@
 
 
 (defn boolean [x]
-  (if (== :false x ) "false" "true" ))
- 
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
+  
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor ) 0)   
+    true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+     :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (if (or (< age 13 )(> age  20))
+    true
+    false))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+       (number? x) (* x 2)
+       (empty? x) nil
+       (or (list? x) (vector? x)) (* 2 (count x))
+       :else true))
 
 (defn leap-year? [year]
-  ":(")
+    
+  (or (divides? 400 year) (and (not (divides? 100 year)) (divides? 4 year)))
+  )
+         
 
 ; '_______'
