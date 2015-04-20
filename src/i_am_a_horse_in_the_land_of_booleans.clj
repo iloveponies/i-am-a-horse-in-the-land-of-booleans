08878d8b4233f9ae653b8f557e6e085cd5e56cae
Stephen Kuenzli <skuenzli@weblogng.com>
2015-04-20 16:17:54 -0700

    implement leap-year?

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -32,6 +32,13 @@
   ":(")
 
 (defn leap-year? [year]
-  ":(")
+;  (divides? 4 year)
+  (cond
+   (divides? 400 year) true
+   (divides? 100 year) false
+   (divides? 4 year) true
+   :else false
+   )
+  )
 
 ; '_______'
