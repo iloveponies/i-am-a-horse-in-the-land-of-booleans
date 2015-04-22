a1a9f8557c9fd9d1e69d5d7ff56ca518a3e918f6
41407 <jimi.welling@cs.helsinki.fi>
2015-04-22 23:47:30 +0300

    Exercise 5

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -18,7 +18,12 @@
   )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 5 n) "buzz"
+   (divides? 3 n) "fizz"
+   :else ""
+   ))
 
 (defn teen? [age]
   (<= 13 age 19))
