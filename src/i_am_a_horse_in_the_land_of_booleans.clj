a496c52324f4c9e820856680a7a69836f75f46ab
henrik.suzuki <henrik.suzuki@arbetsformedlingen.se>
2015-04-22 18:35:52 +0200

    kamemo tobu

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,43 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+    (if (> x 0)
+    x
+    (* x -1)))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor)) true false))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [x]  (cond
+                     (= 0 (mod x 15)) "gotcha!"
+                     (= 0 (mod x 5)) "buzz"
+                     (= 0 (mod x 3)) "fizz"
+                     :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
-(defn not-teen? [age]
-  ":(")
 
-(defn generic-doublificate [x]
-  ":(")
+(defn not-teen? [x]
+  (if  (< 12 x 20)
+    false
+    true
+    ))
 
-(defn leap-year? [year]
-  ":(")
+(defn generic-doublificate [x]  (cond
+                     (number? x) (* 2 x)
+                     (empty? x) nil
+                     (or (list? x) (vector? x)) (* 2 (count x))
+                     :else true))
+
+
+ (defn leap-year? [x]
+  (cond
+   (= 0 (mod x 400)) true
+   (and (= 0 (mod x 4) ) (not(= 0 (mod x 100)))) true
+   :else false))
 
 ; '_______'
