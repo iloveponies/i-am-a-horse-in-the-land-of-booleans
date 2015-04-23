0f32c24f913cfada71a243c899765c7fef013140
Michael <michael@emersea.com>
2015-04-23 19:36:22 -0400

    I have found the (boolean 1)^th

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,45 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x 'true 'false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* -1 x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0) 'true 'false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+        (= (mod n 15) 0) "gotcha!"
+        (= (mod n 5) 0) "buzz"
+        (= (mod n 3) 0) "fizz"
+        1 ""
+        ))
 
 (defn teen? [age]
-  ":(")
+  (if (= (< age 20) (> age 12)) 'true 'false))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)) 
+)
 
 (defn generic-doublificate [x]
-  ":(")
+(cond
+  (number? x) (* 2 x)
+  (empty? x) nil
+  (list? x) (* 2 (count x))
+  (vector? x) (* 2 (count x))
+  1 'true
+))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+	(= (mod year 400) 0) 'true
+	(= (mod year 100) 0) 'false
+	(= (mod year 4) 0) 'true
+	1 'false
+  )
+)
 
-; '_______'
+; '_^~-^~~7<>-+Q>_pQrs_'
