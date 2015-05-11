0b70d8e7227192609c7a4618e1a868db5e415ee3
Kristian Koivisto <kristiak@cs.helsinki.fi>
2015-05-11 22:42:49 +0300

    did them all

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -6,24 +6,43 @@
 
 
 
-(defn abs [x]
-  ":(")
+(defn abs [x] (if (< x 0) (* -1 x) x))
 
-(defn divides? [divisor n]
-  ":(")
+
+(defn divides? [divisor n] (if (== 0 (mod n divisor)) true false))
+
+
+
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   :else ""))
 
 (defn teen? [age] (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (cond
+   (teen? age) false
+   (not (teen? age)) true
+   ))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (list? x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   :else true))
+
+
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (divides? 100 year) (if (divides? 400 year) true false)
+   (divides? 4 year) true
+   :else false))
 
 ; '_______'
