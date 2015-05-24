1d44f6da7617070357f910371c4823378622e4e0
Seeq <Seeq@openmailbox.org>
2015-05-24 01:28:21 -0400

    done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,42 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x] (if (or (= x false) (= x nil))
+                    false
+                    true))
 
-(defn abs [x]
-  ":(")
+(defn abs [x] (if (< x 0)
+                (- x)
+                x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? [divisor n] (if (= 0 (mod n divisor))
+                             true
+                             false))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [n] (cond
+                     (divides? 15 n) "gotcha!"
+                     (divides? 3 n) "fizz"
+                     (divides? 5 n) "buzz"
+                     :else "" ))
 
-(defn teen? [age]
-  ":(")
+(defn teen? [age] ( if (and (<= age 19) (>= age 13))
+                    true
+                    false))  
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? [age] (if (and (<= age 19) (>= age 13))
+                        false
+                        true))
 
-(defn generic-doublificate [x]
-  ":(")
+(defn generic-doublificate [x] (cond (number? x) (* x 2)
+                                     (empty? x) nil
+                                     (vector? x) (* 2 (count x))
+                                     (list? x) (* 2 (count x))
+                                     :else true))
 
-(defn leap-year? [year]
-  ":(")
+(defn leap-year? [year] (cond (= 0 (mod year 400)) true
+                              (= 0 (mod year 100)) false
+                              (= 0 (mod year 4)) true
+                              :else false)
+  )
 
 ; '_______'
