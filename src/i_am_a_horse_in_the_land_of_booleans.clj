33369af8ce8cd4c8f7fec2907f3d8f33a31f2aaa
kadar_akos <kadar.akos@gmail.com>
2015-05-24 11:50:00 +0200

    homework done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,70 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
+
+
+
 (defn boolean [x]
-  ":(")
+  (if x true false))
+
+
 
 (defn abs [x]
-  ":(")
+  (if (> x 0) x (- x)))
+
+
+
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor)))
 
-(defn fizzbuzz [n]
-  ":(")
+
+
+;; The super "cond" experssion 
+(defn fizzbuzz 
+"The fizz buzz"
+  [n]
+ (cond 
+  (== 0 (mod n 15) ) "gotcha!"
+  (== 0 (mod n 5)) "buzz"
+  (== 0 (mod n 3)) "fizz"
+  :else ""
+))
+
+
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
+
+
 
 (defn not-teen? [age]
-  ":(")
+(not (teen? age)) )
+
+
+
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond 
+   (number? x) (* x 2)
+   (empty? x) nil
+   (list? x) (* 2 (count x))
+   (vector? x) (* 2 (count x))
+   "else" true
+))
+
+
 
 (defn leap-year? [year]
-  ":(")
+  (let [div100? (divides? 100 year)
+        div400? (divides? 400 year)]
+    (cond
+      (and div100? (not div400?)) false
+      (divides? 4 year) true
+      (and div100? div400?) true
+      :else false)))
+      
+
+(leap-year? 100)
 
 ; '_______'
