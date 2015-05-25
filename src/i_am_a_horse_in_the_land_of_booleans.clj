494c4d19bcce36b8ebef6faaa72825fccbcf67c5
crustyoldev <crustyoldev@internode.on.net>
2015-05-25 05:30:29 +0000

    All tests completed

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,44 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false)
+)
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* x -1) x)
+)
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0) true false)
+)
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+     (divides? 15 n) "gotcha!"
+     (divides? 5 n) "buzz"
+     (divides? 3 n) "fizz"
+     :else ""))
 
 (defn teen? [age]
-  ":(")
+  (if (>= age 13) (if (<= age 19) true false) false)
+)
 
 (defn not-teen? [age]
-  ":(")
+  (if (not (teen? age)) true false))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+     (number? x) (* x 2)
+     (empty? x) nil
+     (list? x) (* (count x) 2)
+     (vector? x) (* (count x) 2)
+     :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+     (not (divides? 4 year)) false
+     (not (divides? 100 year)) true
+     (not (divides? 400 year)) false
+     :else true))
 
 ; '_______'
