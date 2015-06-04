5d93dae1115d39691ae7a1ebee027e3278e9dfc2
Laura Dean <lgdean@users.noreply.github.com>
2015-06-04 16:25:53 -0400

    exercises 3-6, and discovered some-fn, so that is cool

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -5,13 +5,16 @@
   (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (divides? 15 n) "gotcha!"
+        (divides? 5 n) "buzz"
+        (divides? 3 n) "fizz"
+        :else ""))
 
 (defn teen? [age]
   (<= 13 age 19))
@@ -20,7 +23,10 @@
   ":(")
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* 2 x)
+        (empty? x) nil
+        ((some-fn list? vector?) x) (generic-doublificate (count x))
+        :else true))
 
 (defn leap-year? [year]
   ":(")
