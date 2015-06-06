a6e91e35bac6aa43c73bce1773a50943a3d86f07
choedl <choedl@users.noreply.github.com>
2015-06-06 14:17:51 +0200

    i_am_a_horse_in_the_land_of_booleans

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,48 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
+
 (defn boolean [x]
-  ":(")
+  (if (nil? x)
+    false
+    (if (false? x)
+      false
+      true)))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (= 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (= 0 (mod n 15)) "gotcha!"
+    (= 0 (mod n 3)) "fizz"
+    (= 0 (mod n 5)) "buzz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))) 
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (not= 0 (mod year 4)) false
+    (not= 0 (mod year 100)) true
+    (not= 0 (mod year 400)) false
+    :else true))
+     
 
 ; '_______'
