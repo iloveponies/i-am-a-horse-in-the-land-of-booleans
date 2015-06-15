385f496da20428f5e8309d58c772214441d226b8
xdvarpunen <ismail.belmostefa@gmail.com>
2015-06-15 15:16:47 +0300

    booleans under control

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,70 @@
-(ns i-am-a-horse-in-the-land-of-booleans
+ (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn
+  boolean
+  "returns true or false depending if value is true or nil"
+  [x]
+  (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn
+  abs
+  "returns abs of the given parameter"
+  [x]
+  (if (> x 0)
+    x
+    (* -1 x)))
 
-(defn divides? [divisor n]
-  ":(")
+(defn
+  divides?
+  "returns wether number divides so mod is 0"
+  [divisor n]
+  (if (> (mod n divisor) 0)
+    false
+    true))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn
+  fizzbuzz
+  "returns value depending on parameter like switch case"
+  [n]
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 5 n) "buzz"
+   (divides? 3 n) "fizz"
+   :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn
+  teen?
+  "returns boolean value wether age belongs to teen category"
+  [age]
+  (< 12 age 20))
 
-(defn not-teen? [age]
-  ":(")
+(defn
+  not-teen?
+  "returns wether given age is not-teen"
+  [age]
+  (cond
+   (teen? age) false
+   (not (teen? age)) true))
 
-(defn generic-doublificate [x]
-  ":(")
+(defn
+  generic-doublificate
+  "returns value depending on the parameter given through Clojure kind switch case"
+  [x]
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (list? x) (* (count x) 2)
+   (vector? x) (* (count x) 2)
+   :else true))
 
-(defn leap-year? [year]
-  ":(")
+(defn
+  leap-year?
+  "returns wether given parameter is leap-year or not"
+  [year]
+  (cond
+   (and (divides? 4 year) (divides? 100 year) (divides? 400 year)) true
+   (and (divides? 4 year) (not (divides? 100 year))) true
+   :else false))
 
 ; '_______'
