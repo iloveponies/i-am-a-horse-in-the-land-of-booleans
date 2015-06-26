96c1f4bc407b8c919dc338e126a99b6579a5602f
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2015-06-26 16:09:11 +0300

    exercise 1, 2, 3 and 4

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,32 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x
+    true
+    false))
 
 (defn abs [x]
-  ":(")
+ (if (< x 0)
+    (* -1 x)
+   x
+   )
+)
 
 (defn divides? [divisor n]
-  ":(")
+ (if (< (mod divisor n) 1)
+    false
+   true
+   )
+)
 
 (defn fizzbuzz [n]
   ":(")
 
 (defn teen? [age]
-  ":(")
+ (if (and (>= age 13) (<= age 19))
+    true
+    false)
+)
 
 (defn not-teen? [age]
   ":(")
