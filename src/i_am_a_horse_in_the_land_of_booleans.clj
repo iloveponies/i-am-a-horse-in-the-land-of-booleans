3ce674ff97256165366e29bd7ba88745ae4dcf9b
DeepThree <gaoyuankidult@gmail.com>
2015-06-29 13:11:15 +0300

    finished second exercise

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,45 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x (if nil false true) false)
+)
+
+(defn teen? [x]
+  (if (and (>= x 13) (<= x 19))  true false)
+)
+
+(defn sign [x]
+  (if (< x 0)
+    "-"
+    "+"))
 
 (defn abs [x]
-  ":(")
+  (if (= (sign x) "-") (- x) x)
+)
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? [x y]
+  (if (= (mod y x) 0) true false)
+)
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [number]
+  (cond
+    (divides? 15 number) "gotcha!"
+    (divides? 3 number) "fizz"
+    (divides? 5 number) "buzz"
+    :else         ""))
 
-(defn teen? [age]
-  ":(")
+(defn generic-doublificate [input]
+  (cond
+    (number? input) (* input 2)
+    (empty? input) nil
+    (list? input) (* (count input) 2)
+    (vector? input) (* (count input) 2)
+    :else         true))
 
 (defn not-teen? [age]
-  ":(")
-
-(defn generic-doublificate [x]
-  ":(")
+  (not (teen? age))
+  )
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (if (divides? 400 year) true (if (divides? 100 year) false (if (divides? 4 year) true false)  ))
+  )
