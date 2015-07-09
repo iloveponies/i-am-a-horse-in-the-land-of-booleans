30ff568eca2d0f136e2c1b5857852c3fc9417ac5
blobbered <blobbered@gmail.com>
2015-07-09 11:25:20 -0500

    gentle introduction to booleans and conditionals

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,44 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (not (or (nil? x) (= false x))))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+   (== 0 (mod n 15)) "gotcha!"
+   (== 0 (mod n 5)) "buzz"
+   (== 0 (mod n 3)) "fizz"
+   :else ""
+))
 
 (defn teen? [age]
-  ":(")
+  (and (> age 12) (< age 20)))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (* x 2)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true)
+)
 
 (defn leap-year? [year]
-  ":(")
+  (cond 
+   (== 0 (mod year 400)) true
+   (== 0 (mod year 100)) false
+   (== 0 (mod year 4)) true
+   :else false
+))
 
 ; '_______'
