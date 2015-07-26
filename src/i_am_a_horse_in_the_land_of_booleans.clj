25662cd19a60d799d5fb004202a7df9cd659f8c9
Henri Malkki <henri@mbair.local>
2015-07-26 14:54:45 +0300

    update

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,21 +2,36 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (= x nil)
+  	(= false)
+  	(= true)
+  )
+)
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+  	(- x)
+  	x
+  )
+)
 
 (defn divides? [divisor n]
-  ":(")
+  (if (mod n divisor) 0)
+  	(= true)
+  	(= false)
+)
 
 (defn fizzbuzz [n]
   ":(")
 
 (defn teen? [age]
-  ":(")
+  (if (and (>= age 13) (<= age 19) true)
+  	( = true)
+  	( = false)
+  )
+)
 
-(defn not-teen? [age]
+(defn not-teen? [age]	
   ":(")
 
 (defn generic-doublificate [x]
