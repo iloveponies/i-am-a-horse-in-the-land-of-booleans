3d50e4bf4914ae57fdffb440b45aee6ab6e38b8f
Brian van de Boogaard (bv031773) <Brian.vandeBoogaard@Cerner.com>
2015-08-08 20:10:29 -0500

    Implement solutions

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,41 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or (not x) (nil? x))
+    false
+    true))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (* x -1)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (zero? (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (zero? (mod n 3)) "fizz"
+    (zero? (mod n 5)) "buzz"
+    (zero? (mod n 15)) "gotcha!"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (and (<= 13 age) (<= age 19)))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (cond
+    (= (and (mod year 100) (mod year 400)) 0) true
+    (= (mod year 100) 0) false
+    (= (mod year 4) 0) true
+    :else false))
