62dfe638dafc247150905ca59621f65d493d64c7
Markus Kettunen <markus.kettunen@affecto.com>
2015-08-17 14:37:42 +0300

    Tests

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,39 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (= (mod n 15) 0)   "gotcha!"
+    (= (mod n 5) 0)    "buzz"
+    (= (mod n 3) 0)    "fizz"
+    :else              ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x)                  (+ x x)
+    (empty? x)                   nil
+    (or (list? x) (vector? x))  (* (count x) 2)
+    :else                        true))
 
 (defn leap-year? [year]
-  ":(")
+   (cond
+    (= (mod year 400) 0)   true
+    (= (mod year 100) 0)   false
+    (= (mod year 4) 0)     true
+    :else                  false))
 
-; '_______'
+; '__\o/__'
