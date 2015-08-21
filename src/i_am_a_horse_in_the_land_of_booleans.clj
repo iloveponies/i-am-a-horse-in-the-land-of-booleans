7cca2a37373ce238090f740198a5f709721b035d
Bjorn Andrist <bjorn.andrist@gmail.com>
2015-08-21 09:52:34 +0200

    Solutions to boolean exercises

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,54 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false)) 
 
-(defn abs [x]
-  ":(")
+(defn abs [x] 
+  (if (< x 0) 
+    (* x -1) 
+    x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? [divisor n] 
+  (if (= (mod n divisor) 0) 
+    true 
+    false))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [n] 
+  (cond 
+    (= (mod n 45) 0) "gotcha!"
+    (= (mod n 5) 0) "buzz"
+    (= (mod n 3) 0) "fizz"
+    :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen? [age] (<= 13 age 19))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? [age] (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond 
+    (number? x) (+ x x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x)) 
+    :else true))
+
+;(defn leap-year? [year]
+;  (if (= (mod year 4) 0)
+;    (if (and (= (mod year 100) 0) (not (= (mod year 400) 0)))
+;      false
+;      true)
+;    false))
+
+
+;if (year is not exactly divisible by 4) then (it is a common year)
+;else
+;if (year is not exactly divisible by 100) then (it is a leap year)
+;else
+;if (year is not exactly divisible by 400) then (it is a common year)
+;else (it is a leap year)
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (cond 
+    (not (= (mod year 4) 0)) false
+    (not (= (mod year 100) 0)) true
+    (not (= (mod year 400) 0)) false
+    :else true))
