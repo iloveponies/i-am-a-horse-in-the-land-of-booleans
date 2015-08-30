54efa88a8dde1174e9e558df5df6d2f95502fef1
Henri Malkki <henri.malkki@cs.helsinki.fi>
2015-08-30 13:00:29 +0300

    all dones

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -18,13 +18,16 @@
 )
 
 (defn divides? [divisor n]
-  (if (mod n divisor) 0)
-  	(= true)
-  	(= false)
-)
+  (zero? (mod n divisor))
+) 
 
-(defn fizzbuzz [n]
-  ":(")
+
+(defn fizzbuzz [n] 
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
 (defn teen? [age]
   (cond (< age 13) false
@@ -39,9 +42,22 @@
 )
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (vector? x) (* 2 (count x))
+    (list? x) (* 2 (count x))
+    :else true
+  )  
+)
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false
+  )
+)
 
 ; '_______'
