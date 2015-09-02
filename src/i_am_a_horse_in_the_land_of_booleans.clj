48cacd4fc714e1e4d6490a0998f9114ae5aac9ee
Petri Hakola <petri.hakola@iki.fi>
2015-09-02 22:04:45 +0300

    Almost there.

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,36 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* -1 x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod divisor n) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond 
+   (divides? n 15) "gotcha" 
+   (divides? n 3) "fizz" 
+   (divides? n 5) "buzz" 
+   :else ""))
+
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
   ":(")
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+   (divides? year 400) true
+   (divides? year 100) false
+   (divides? year 4) true
+   :else false))
 
 ; '_______'
