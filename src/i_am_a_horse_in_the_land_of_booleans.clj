58977053dce9b5e290339a25deb14493f7c4d512
U-ELI\Eli <eliseegs@hotmail.com>
2015-09-12 15:59:34 +1000

    finished

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,42 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
-
-(defn abs [x]
-  ":(")
-
-(defn divides? [divisor n]
-  ":(")
-
-(defn fizzbuzz [n]
-  ":(")
+  (if x true false))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
+
+(defn divides? [divisor n]
+  (= (mod n divisor) 0))
+
+(defn fizzbuzz [n]
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 3 n) "fizz"
+   (divides? 5 n) "buzz"
+   :else ""))
+
+(defn abs [numb]
+  (if (< numb 0)
+    (- numb)
+    numb))
+
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
+
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+   (number? x) (+ x x)
+   (or (list? x) (vector? x)) (* 2 (count x))
+   (and (coll? x) (empty? x)) nil
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (or (and (divides? 4 year) (not (divides? 100 year)))
+      (and (divides? 4 year) (divides? 400 year))))
+
+
 
 ; '_______'
