528ce8c031abe8371cb38121113053543bf8f252
Mika Turunen <mika.turunen@ymail.com>
2015-09-17 10:37:11 +0300

    Not a teen and teen functions.

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,8 +1,16 @@
 (ns i-am-a-horse-in-the-land-of-booleans
-  (:refer-clojure :exclude [boolean]))
+    (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+    ((if x nil)
+        ("falsy")
+
+        ((if x false)
+            ("falsy")
+            ("truthy")
+        )
+    )
+)
 
 (defn abs [x]
   ":(")
@@ -14,10 +22,12 @@
   ":(")
 
 (defn teen? [age]
-  ":(")
+    (and (<= age 19) (>= age 13))
+)
 
 (defn not-teen? [age]
-  ":(")
+    (not (teen? age))
+)
 
 (defn generic-doublificate [x]
   ":(")
