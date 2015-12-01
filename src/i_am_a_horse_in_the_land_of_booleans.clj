daed13e4b78a05c88f374ea308b4b5422984159e
JohnFoobar13 <johnfoobar13@gmail.com>
2015-12-01 09:51:38 +0100

    all tests passed

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,65 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  ( if x true false )
+)
 
 (defn abs [x]
-  ":(")
+  ( if (< x 0) (* -1 x) (* 1 x) )
+
+)
 
 (defn divides? [divisor n]
-  ":(")
+  ( if (== (mod n divisor) 0)
+    true
+    false
+  )
+)
+
+(defn devisible_by_3? [n]
+  (divides? 3 n)
+)
+
+(defn devisible_by_5? [n]
+  (divides? 5 n)
+)
+
+(defn devisible_by_15? [n]
+  (divides? 15 n)
+)
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (devisible_by_15? n) "gotcha!"
+    (devisible_by_3? n)  "fizz"
+    (devisible_by_5? n)  "buzz"
+    :else                ""
+  )
+)
 
 (defn teen? [age]
-  ":(")
+    ( <= 13 age 19 )
+)
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+)
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x)  nil
+    (list? x)   ( * 2 (count x) )
+    (vector? x) ( * 2 (count x) )
+    :else       true
+  )
+)
 
 (defn leap-year? [year]
-  ":(")
+  (cond (divides? 400 year) true
+        (divides? 100 year) false
+        (divides? 4 year)   true
+        :else               false)
+)
 
 ; '_______'
