90927d3dcea0ff27db4265a2069216bc1128840a
Teemu Salminen <teemu.salminen@intelle.fi>
2015-12-17 15:51:45 +0200

    WIP

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,23 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false ))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (* x -1) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+     (divides? 15 n) "gotcha!"
+     (divides? 5 n) "buzz"
+     (divides? 3 n) "fizz"
+     :else ""))
 
 (defn teen? [age]
-  ":(")
+  (if (<= age 19) (if (>= age 13) true false) false))
 
 (defn not-teen? [age]
   ":(")
