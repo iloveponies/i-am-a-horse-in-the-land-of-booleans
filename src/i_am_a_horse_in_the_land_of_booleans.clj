2e7e82c4f6573626e5561ef1297f3cd519975c4d
Tim Chan <tchan@cargomatic.com>
2015-12-24 15:42:56 -0800

    tests pass

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,41 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
+
 (defn boolean [x]
-  ":(")
+  (if x true false))
+
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (* -1 x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
-
-(defn teen? [age]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+   :else ""))
 
 (defn not-teen? [age]
-  ":(")
+  (or (< age 13) (> age 19)))
+
+(defn teen? [age]
+  (not (not-teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+  :else true))
+
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (or
+    (and (divides? 4 year) (not (divides? 100 year)))
+    (and (divides? 100 year) (divides? 400 year))))
