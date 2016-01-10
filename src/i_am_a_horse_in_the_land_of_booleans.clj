b3a0399c5c8f8580c6c367f3a6c106cdbc336f20
Gordon J. Broom <gjbroom@thebrooms.ca>
2016-01-09 22:51:59 -0800

    another exercise

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,38 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn abs "Absolute val" [val]
+  (if (< val 0)
+    (- val)
+    val))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? "Does DIVISOR divide N?" [divisor n]
+  (= 0 (mod n divisor)))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz "Fizzbuzz test" [n]
+  (cond
+   (= 0 (mod n 15)) "gotcha!"
+   (= 0 (mod n 3)) "fizz"
+   (= 0 (mod n 5)) "buzz"
+   :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen? "Is AGE a teenager?" [age] (< 12 age 20))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? [age] (not (teen? age)))
 
-(defn generic-doublificate [x]
-  ":(")
+(defn generic-doublificate "exercise 6" [x]
+  (cond
+   (number? x) (* 2 x)
+   (empty? x) nil
+   (or (list? x) (vector? x)) (* 2 (count x))
+   :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (or (= 0 (mod year 400))
+      (and (= 0 (mod year 4))
+           (not (= 0 (mod year 100))))))
+
 
 ; '_______'
