85a4692db7087779aab76f2d7acbcdfac43cc97e
James Wheaton <xhtmlgeek@gmail.com>
2016-02-24 21:55:19 -0700

    Exercise 8: leap-year?/1

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -36,6 +36,10 @@
     :else                      true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year)   true
+    :else               false))
 
 ; '_______'
