dd5c9b61af3a3376f0673af570c7956d56b57fca
James Wheaton <xhtmlgeek@gmail.com>
2016-02-24 20:33:38 -0700

    Make divides?/2 more readable

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -8,7 +8,12 @@
   (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  (if (== 0 (mod n divisor)) true false))
+  (if
+    (==
+      0
+      (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
   (cond
