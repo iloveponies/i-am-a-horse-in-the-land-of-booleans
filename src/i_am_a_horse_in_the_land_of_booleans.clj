59e9dbcad73d7a86bac1bf7b4dd871b969ff0c21
Dinesh Weerapurage <xydinesh@gmail.com>
2016-02-26 01:21:35 -0700

    Finished 8 excercises of second task

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -7,24 +7,42 @@
 
 
 (defn abs [x]
-  ":(")
+    "Get absolute value of the value"
+    (if (< x 0)
+        (* -1 x)
+        x))
 
 (defn divides? [divisor n]
-  ":(")
+  "Check for divides"
+  (if (= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+    "Fizzbuzz 3, 5, 15"
+    (cond
+        (= (mod n 15) 0) "gotcha!"
+        (= (mod n 5) 0) "buzz"
+        (= (mod n 3) 0) "fizz"
+        :else ""))
 
 (defn teen? [age]
-  ":(")
+    "Determine age is between 13 and 19"
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+    (cond
+        (number? x) (* 2 x)
+        (empty? x) nil
+        (list? x) (* 2 (count x))
+        (vector? x) (* 2 (count x))
+        :else true ))
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+    "Determine year is a leap year"
+    (cond
+        (= 0 (mod year 400)) true
+        (= 0 (mod year 100)) false
+        (= 0 (mod year 4)) true
+        :else false ))
