5e7baa9ca919d2c19724423e6ac46ef2adbf6ec4
Toni Laroma <toni.laroma@houston-inc.com>
2016-04-04 22:55:07 +0300

    done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,91 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
+
+(boolean "foo") ;=> true
+(boolean nil) ;=> false
+(boolean (+ 2 3)) ;=> true
+(boolean true) ;=> true
+(boolean false) ;=> false
 
 (defn abs [x]
-  ":(")
+  (if (neg? x) (* -1 x) (* 1 x)))
+
+(abs -2) ;=> 2
+(abs 42) ;=> 42
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== 0 (mod n divisor)) true false))
+
+(divides? 2 4) ;=> true
+(divides? 4 2) ;=> false
+(divides? 5 10) ;=> true
+(divides? 2 5) ;=> false
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
+
+(fizzbuzz 2)  ;=> ""
+(fizzbuzz 45) ;=> "gotcha!"
+(fizzbuzz 48) ;=> "fizz"
+(fizzbuzz 70) ;=> "buzz"
 
 (defn teen? [age]
-  ":(")
+  (if (<= 13 age 19) true false))
+
+(teen? 12) ;=> false
+(teen? 13) ;=> true
+(teen? 15) ;=> true
+(teen? 19) ;=> true
+(teen? 20) ;=> false
+(teen? 27) ;=> false
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
+
+(not-teen? 13) ;=> false
+(not-teen? 25) ;=> true
+(not-teen? 12) ;=> true
+(not-teen? 19) ;=> false
+(not-teen? 20) ;=> true
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else true))
+
+(generic-doublificate 1)        ;=> 2
+(generic-doublificate [1 2])    ;=> 4
+(generic-doublificate '(65 21)) ;=> 4
+(generic-doublificate {})       ;=> nil
+(generic-doublificate [])       ;=> nil
+(generic-doublificate {:a 1})   ;=> true
+
 
 (defn leap-year? [year]
-  ":(")
+  (and
+    (or
+      (divides? 4 year)
+      (divides? 400 year))
+    (or
+      (not (divides? 100 year))
+      (divides? 400 year))))
+
+(leap-year? 100) ;=> false
+(leap-year? 200) ;=> false
+(leap-year? 400) ;=> true
+(leap-year? 12)  ;=> true
+(leap-year? 20)  ;=> true
+(leap-year? 15)  ;=> false
+
+
 
 ; '_______'
