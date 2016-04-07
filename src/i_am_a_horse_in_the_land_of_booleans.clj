d057752ccea4ac18683a01fcad93cb7ade700e4f
emiguelt <edwin.triana@bizagi.com>
2016-04-07 09:15:28 -0500

    cljmarathon - partial commit

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,25 +2,31 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond (divides? 3 n) "fizz"
+        (divides? 5 n) "buzz"
+        (divides? 15 n) "gotcha!"
+        :else ""))
 
 (defn teen? [age]
-  ":(")
+  (< 13 age 19))
 
 (defn not-teen? [age]
   ":(")
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond (number? x) (* 2 x)
+        (or (list? x) (vector? x))
+            (if (empty? x) nil (*2 (count x))))
+        :else true)
 
 (defn leap-year? [year]
   ":(")
