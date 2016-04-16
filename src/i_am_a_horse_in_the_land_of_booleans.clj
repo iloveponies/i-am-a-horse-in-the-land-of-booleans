381f5accc03f7be8c53986f05730f71bade5d1d6
Daniel Lillqvist <daniel.lillqvist@helsinki.fi>
2016-04-16 21:08:41 +0300

    Horses all the way up

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,45 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0) true false))
+
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (== (mod n 15) 0) "gotcha!"
+    (== (mod n 5) 0) "buzz"
+    (== (mod n 3) 0) "fizz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (if (= (>= age 13) (<= age 19)) true false))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x) nil
+    (list? x) (* (count x) 2)
+    (vector? x) (* (count x) 2)
+    :else true
+  )
+)
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (not (== (mod year 4) 0)) false
+    (not (== (mod year 100) 0)) true
+    (not (== (mod year 400) 0)) false
+    :else true
+  )
+)
 
 ; '_______'
