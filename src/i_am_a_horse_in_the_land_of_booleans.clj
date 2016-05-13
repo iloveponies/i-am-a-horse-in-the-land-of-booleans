13eada42897510460e4851d0e1308d9a92691417
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-05-13 07:12:08 +0300

    answers to set exercises

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,53 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x] (if x true false))
+
+(boolean true)
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- 0 x) x) )
 
 (defn divides? [divisor n]
-  ":(")
+  (== 0 (mod n divisor) ) )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (== 0 (mod n 3) (mod n 5) ) "gotcha!"
+    (== 0 (mod n 3) ) "fizz"
+    (== 0 (mod n 5) ) "buzz"
+    :else ""
+    ))
 
 (defn teen? [age]
-  ":(")
+   (<= 13 age 19) )
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
+
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x) nil
+    (or (vector? x) (list? x)) (* (count x) 2)
+    :else true
+))
+
+(generic-doublificate {:a 1})
+
+
+(defn divisible-with [diviser value]
+  (== 0 (mod value diviser)))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (and (divisible-with 100 year) (not (divisible-with 400 year))) false
+    (divisible-with 4 year) true
+    :else false)
+  )
+
+(leap-year? 100)
+
 
 ; '_______'
