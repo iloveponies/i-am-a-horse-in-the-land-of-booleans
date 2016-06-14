3eda90b6f68388de6503b4dd5fbf986e0928becc
Jorard <jorard@odmedia.nl>
2016-06-14 21:29:37 +0200

    fizzbuzz!

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,22 +2,26 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (neg? x) (* x -1) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (== (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (if (< age 13) true (if (> age 19) true false)))
 
 (defn generic-doublificate [x]
   ":(")
