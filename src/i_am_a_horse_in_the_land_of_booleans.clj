5f7c5afa38044822d0ab3b71dab3f0d5c687ff40
naironics <naironics@gmail.com>
2016-06-19 16:48:54 -0700

    exercise 8 tests passed

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -31,6 +31,10 @@
     :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond 
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false ))
 
 ; '_______'
