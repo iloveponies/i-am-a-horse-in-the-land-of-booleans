c169cb2be385c034a33092e9c50c1a6ebf152e50
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-07-11 01:55:48 +0300

    moi

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,42 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x] (if x (= x x) (= x 1)))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if-not (= 0 (mod n divisor)) false true))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 3 n) (if (divides? 5 n) "gotcha!" "fizz")
+    (divides? 5 n) "buzz"
+    :else ""
+    ))
 
-(defn teen? [age]
-  ":(")
+(defn teen? [age] (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else true
+  ))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 4 year) (if (divides? 100 year) (divides? 400 year) false)
+    :else false
+    ))
 
 ; '_______'
+
+
+(leap-year? 2100)
