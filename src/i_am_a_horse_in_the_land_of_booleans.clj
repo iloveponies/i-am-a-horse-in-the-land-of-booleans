9acf57a2858dc1f2d0b82e5b765db22f293c21b8
Tania Sierra <tania.sierra@helsinki.fi>
2016-07-13 19:06:56 +0300

    land of booleans tehty

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,38 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else         ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (and (divides? 4 year) (not (divides? 100 year))) true
+    (and (divides? 4 year) (divides? 100 year) (divides? 400 year)) true
+    :else false))
 
 ; '_______'
