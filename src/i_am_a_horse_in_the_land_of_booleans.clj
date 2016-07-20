053da8eca7b796b08eaa8af8c394da4e5c55237a
tleht <toni.lehtonen7@gmail.com>
2016-07-20 19:02:47 +0300

    Tokat tehtävät

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,39 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  if(x true false))
 
 (defn abs [x]
-  ":(")
+  if((< 0 x) (* -1 x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  if((= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  cond((divides? 15) "gotcha"
+       (divides? 3) "fizz"
+       (divides? 5) "buzz"
+       :else ""))
 
 (defn teen? [age]
-  ":(")
+  if((< 12 age 20) true false)
 
 (defn not-teen? [age]
-  ":(")
+  not(teen? age))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false))
 
 ; '_______'
+
