0c70621f495d72c154ae7569e6b743f1444522ec
info2016 <niquet_xavier2002@yahoo.fr>
2016-08-02 16:17:08 +0200

    clojure chapter 1

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,70 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  "return true if x = foo "
+  (if (or (= x false )(= x nil))
+    false
+    true))
 
 (defn abs [x]
-  ":(")
+  (if (> 0 x)
+    (* -1 x)
+    x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides?
+  [divisor n]
+  "return true if dividor divide n"
+  (if (= 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
-
+  "(int)-> boolean"
+  (cond 
+    (divides? 15 n) "gotcha!"
+    (divides? 5 n) "buzz"
+    (divides? 3 n) "fizz"
+     :else ""))
+    
+  
+(def liste [2 45 48 70])
+(defn test
+  [function liste]
+  (map function liste))
+  
 (defn teen? [age]
-  ":(")
+  "return true if 13<age<19"
+  (if (and (<= 13 age) (>= 19 age))
+    true
+    false))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x ) (* 2 x)
+    (empty? x) nil
+    (or (list? x)(vector? x) ) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  "(int)->boolean"
+  (cond 
+    (= 0 (mod year 100) 
+       (if (= 0 (mod year 400))
+         true
+         false))
+   (= 0 (mod year 4)
+    true
+    :else false)))
+ 
 
-; '_______'
+(def liste-5 [2 45 48 70])
+(def liste-6 [1 [1 2] '(65 21) {} [] {:a 1}])
+(def liste-7 [13 25 12 19 20])
+(def liste-8 [100 200 400 12 20 15])
+(defn my-test
+  [function liste]
+  (map function liste)) 
+
