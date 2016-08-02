2f1abc9c4b14cab87dc9d2090cf7c253247146f3
info2016 <niquet_xavier2002@yahoo.fr>
2016-08-02 16:49:19 +0200

    chapter 2 clojure

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -7,6 +7,13 @@
     false
     true))
 
+;; (boolean "foo") ;=> true
+;; (boolean nil) ;=> false
+;; (boolean (+ 2 3)) ;=> true
+;; (boolean true) ;=> true
+;; (boolean false) ;=> false
+
+
 (defn abs [x]
   (if (> 0 x)
     (* -1 x)
@@ -21,18 +28,18 @@
 
 (defn fizzbuzz [n]
   "(int)-> boolean"
-  (cond 
+  (cond
     (divides? 15 n) "gotcha!"
     (divides? 5 n) "buzz"
     (divides? 3 n) "fizz"
      :else ""))
-    
-  
+
+
 (def liste [2 45 48 70])
 (defn test
   [function liste]
   (map function liste))
-  
+
 (defn teen? [age]
   "return true if 13<age<19"
   (if (and (<= 13 age) (>= 19 age))
@@ -51,21 +58,30 @@
 
 (defn leap-year? [year]
   "(int)->boolean"
-  (cond 
-    (= 0 (mod year 100) 
-       (if (= 0 (mod year 400))
-         true
-         false))
-   (= 0 (mod year 4)
-    true
-    :else false)))
- 
+  (cond
+    (= 0 (mod year 100))  (if (= 0 (mod year 400))
+                            true
+                            false)
 
-(def liste-5 [2 45 48 70])
-(def liste-6 [1 [1 2] '(65 21) {} [] {:a 1}])
-(def liste-7 [13 25 12 19 20])
-(def liste-8 [100 200 400 12 20 15])
-(defn my-test
-  [function liste]
-  (map function liste)) 
+   (= 0 (mod year 4)) true
+  :else false)
+  )
+
+
+;;  (leap-year? 100) ;=> false
+;;  (leap-year? 200) ;=> false
+;; (leap-year? 400);=> true
+;;   (leap-year? 12)  ;=> true
+;;   (leap-year? 20)  ;=> true
+;;   (leap-year? 15)  ;=> false
+
+
+
+;; (def liste-5 [2 45 48 70])
+;; (def liste-6 [1 [1 2] '(65 21) {} [] {:a 1}])
+;; (def liste-7 [13 25 12 19 20])
+;; (def liste-8 [100 200 400 12 20 15])
+;; (defn my-test
+;;   [function liste]
+;;   (map function liste))
 
