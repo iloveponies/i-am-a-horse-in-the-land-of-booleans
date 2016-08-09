e553529068e743c5567c8c61be31ac33e6476129
Hiraki <hiraki@oasharp.co.jp>
2016-08-09 21:55:15 +0900

    exercise 8

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -39,6 +39,11 @@
     :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (zero? (mod year 400)) true
+    (zero? (mod year 100)) false
+    (zero? (mod year 4)) true
+    :else false))
+
 
 ; '_______'
