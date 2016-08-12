409d39117030a02710e62ee9b37193ae1e617875
Saugat Awale <saugata@metropolia.fi>
2016-08-12 15:54:32 +0300

    exercise 10

--- MainActivity.java
+++ MainActivity.java
@@ -1,55 +1,13 @@
-package com.example.dell.exercise1;
+package com.example.dell.exercise10;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private EditText firstText;
-    private EditText secondText;
-    private Button firstButton;
-    private Button secondButton;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-    }
-
-    public void init(){
-        firstText = (EditText) findViewById(R.id.first_text);
-        secondText = (EditText) findViewById(R.id.second_text);
-        firstButton=(Button) findViewById(R.id.first_button);
-        secondButton=(Button) findViewById(R.id.second_button);
-        firstButton.setOnClickListener(this);
-        secondButton.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view){
-        switch (view.getId()){
-            case R.id.first_button:
-                Intent intent1 = new Intent(this,Activity2.class);
-
-                startActivity(intent1);
-                finish();
-                break;
-
-            case R.id.second_button:
-                Intent intent2 = new Intent(this,Activity3.class);
-                String text1 = firstText.getText().toString();
-                String text2 = secondText.getText().toString();
-                intent2.putExtra("parameter1",text1);
-                intent2.putExtra("parameter2",text2);
-                startActivity(intent2);
-
-                break;
-        }
-
     }
 }
