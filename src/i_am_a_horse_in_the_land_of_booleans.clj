24523188042770755386873e0aa4549d8926847b
Markus Juopperi <markus.juopperi@kolumbus.fi>
2016-10-13 13:30:53 +0300

    Solve generic-doublificate

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -24,7 +24,11 @@
   (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
   ":(")
