9740052e855cb8b36bcc0ad6af94a10eecb31ed7
styleza <ilari.richardt@helsinki.fi>
2016-10-22 14:33:22 +0300

    done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,41 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
+  
+;  Horses Ex1
+(defn boolean "Get boolean" [x] (if x true false))
 
-(defn boolean [x]
-  ":(")
+;  Horses Ex2
+(defn teen? "Is teen?" [x] (if (<= x 19) (>= x 13) false))
 
-(defn abs [x]
-  ":(")
+;  Horses Ex3
+(defn abs "Abs value of x" [x] (if (< x 0) (* -1 x) x))
 
-(defn divides? [divisor n]
-  ":(")
+;  Horses Ex4
+(defn divides? "Divides" [x, y] (= 0 (mod y x)))
 
-(defn fizzbuzz [n]
-  ":(")
+;  Horses Ex5
+(defn fizzbuzz "Fizzbuzz" [n] (cond
+	(divides? 15 n) "gotcha!"
+	(divides? 5 n) "buzz"
+	(divides? 3 n) "fizz"
+	:else "")
+)
 
-(defn teen? [age]
-  ":(")
+; Horses Ex6
+(defn generic-doublificate "Geneerix" [x] (cond
+	(number? x) (* x 2)
+	(empty? x) nil
+	(vector? x) (* 2 (count x))
+	(list? x) (* 2 (count x))
+	:else true
+))
 
-(defn not-teen? [age]
-  ":(")
+; Horses Ex7
+(defn not-teen? "not teen" [age] (not (teen? age)))
 
-(defn generic-doublificate [x]
-  ":(")
-
-(defn leap-year? [year]
-  ":(")
+; Horses Ex8
+(defn leap-year? "is leap year" [year] (and (divides? 4 year) (or (not (divides? 100 year)) (divides? 400 year))))
 
 ; '_______'
+
+
