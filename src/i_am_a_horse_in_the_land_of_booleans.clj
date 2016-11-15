ae8f5e848e16b7942624b558b2ac06dc0fb3c6ef
Aleksandr Tsumel <alex.tchumel@gmail.com>
2016-11-15 22:50:40 +0200

    Done ex. 6 and 8

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -28,9 +28,17 @@
   (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond 
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (and
+    (divides? 4 year) 
+    (or 
+      (not (divides? 100 year)) 
+      (divides? 400 year))))
 
 ; '_______'
