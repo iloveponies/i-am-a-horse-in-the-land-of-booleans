e98571bf1a6283f86e1d66db42ee0ef8bbe7b200
Ilmo Raunio <ilmo.raunio@solita.fi>
2016-11-23 15:51:07 +0200

    Add implementation + extra test

--- test/i_am_a_horse_in_the_land_of_booleans_test.clj
+++ test/i_am_a_horse_in_the_land_of_booleans_test.clj
@@ -1,2 +1,7 @@
 (ns i-am-a-horse-in-the-land-of-booleans-test
-  (:use iloveponies.tests.i-am-a-horse-in-the-land-of-booleans))
+  (:use i-am-a-horse-in-the-land-of-booleans
+        iloveponies.tests.i-am-a-horse-in-the-land-of-booleans
+        midje.sweet))
+
+(facts "generic-doublificate (additional)"
+  (generic-doublificate nil) => nil)
