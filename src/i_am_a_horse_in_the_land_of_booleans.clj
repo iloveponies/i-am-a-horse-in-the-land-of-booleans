37ea9ceea055f77f90b547e2d8cc247969202b1d
Sergio Velasco <serveba@prot-os.com>
2016-12-09 20:50:36 +0100

    solved

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,57 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
+
 (defn boolean [x]
-  ":(")
+  (if (nil? x)
+   false
+   (if (false? x)
+     false
+     true)))
 
-(defn abs [x]
-  ":(")
-
-(defn divides? [divisor n]
-  ":(")
-
-(defn fizzbuzz [n]
-  ":(")
 
 (defn teen? [age]
-  ":(")
+  (if (<= age 19)
+    (if (>= age 13)
+      true
+      false)
+    false))
+
+(defn abs [x]
+  (if (< x 0)
+    (* -1 x)
+    x))
+
+
+(defn divides? [divisor n]
+  (if (= (mod n divisor) 0)
+    true
+    false))
+
+
+(defn fizzbuzz [n]
+  (cond
+    (= (mod n 15) 0) "gotcha!"
+    (= (mod n 3) 0) "fizz"
+    (= (mod n 5) 0) "buzz"
+    :else ""))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x) nil
+    (list? x) (* (count x) 2)
+    (vector? x) (* (count x) 2)
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (if (= (mod year 400) 0)
+    true
+    (if (= (mod year 100) 0)
+     false
+     (if (= (mod year 4) 0)
+      true
+      false))))
