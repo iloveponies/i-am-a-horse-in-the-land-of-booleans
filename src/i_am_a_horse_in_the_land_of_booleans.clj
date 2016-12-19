b0ae04f96447931f358e7ef8226a0802df9b12eb
Heikki Holopainen <heikki.holopainen@gmail.com>
2016-12-20 00:30:57 +0200

    Two more to go.

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,22 +2,46 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if
+    x
+    true
+    false))
 
 (defn abs [x]
-  ":(")
+  (if
+    (> x 0)
+    x
+    (- x))
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (if
+    (= 0 (mod n divisor))
+    true
+    false
+    ))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else
+    ""))
 
 (defn teen? [age]
-  ":(")
+  (if
+    (=
+      (<= age 19)
+      (>= age 13)
+      )
+    true
+    false
+    ))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))
+  )
 
 (defn generic-doublificate [x]
   ":(")
