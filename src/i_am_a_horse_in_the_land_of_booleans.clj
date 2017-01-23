607c78a2fc07e3467c831611170b5a16d401fe74
stphnr <stphnr@users.noreply.github.com>
2017-01-23 03:50:28 -0600

    Exercises completed

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,43 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean "implements built-in boolean function w/o said function" [x]
+  (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn abs "returns the absolute value of a number" [x]
+  (if (> x 0)
+    x
+	  (* x -1)))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? "tests whether division produces no remainder" [divisor n] (== (mod n divisor) 0))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz "returns a string based on divisibility" [n]
+  (cond
+    (== 0 (mod n 15)) "gotcha!"
+    (== 0 (mod n 5)) "buzz"
+    (== 0 (mod n 3)) "fizz"
+    :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen? "returns truthy if age btwn 13-19 inclusive" [age]
+  (<= 13 age 19))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? "returns true if not teen, false if teen" [age]
+  (if (not (teen? age))
+    true
+    false))
 
-(defn generic-doublificate [x]
-  ":(")
+(defn generic-doublificate "dbls number, nil if {}, dbls list/vector length, or true" [x]
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else true))
 
-(defn leap-year? [year]
-  ":(")
+(defn leap-year? "determines if given year is a leap year" [year]
+  (cond
+    (and (== 0 (mod year 4)) (< 0 (mod year 100))) true
+    (and (== 0 (mod year 4)) (== 0 (mod year 400))) true
+    :else false))
 
 ; '_______'
