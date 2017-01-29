5e3a004835249c724904ba7fdf1b63a7e94f9e23
Minttu Hurme <minttu.hurme@helsinki.fi>
2017-01-29 22:24:28 +0200

    Exercises 2

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,61 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean "Imitates built-in function boolean" [x]
+  (if x true false)
+  )
 
-(defn abs [x]
-  ":(")
+(defn abs "Return absolute value of a number" [x]
+  (if (<= 0 x) x (- 0 x) ))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? "Does divisor divide n exactly?" [divisor n]
+  (if
+    (= 0 (mod n divisor))
+    true
+    false))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz
+  "returns fizz when n is divisible by 3, buzz when n is divisible by 5, gotcha! when n is divisible by 15, and the empty string otherwise."
+  [n]
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen? "Tells whether age is of a teenager" [age]
+  (< 12 age 20))
 
-(defn not-teen? [age]
-  ":(")
+(defn
+  not-teen?
+  "return false for teeny ages, true for others"
+  [age]
+  (not (teen? age))
+  )
 
-(defn generic-doublificate [x]
-  ":(")
+(defn
+  generic-doublificate
+  "Does doublification based on inputs type"
+  [x]
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else true))
 
-(defn leap-year? [year]
-  ":(")
+
+(defn leap-year?
+  "Is given year a leap year?"
+  [year]
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false)
+
+    )
+
+
 
 ; '_______'
