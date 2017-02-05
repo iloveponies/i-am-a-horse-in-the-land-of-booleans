be18b11de2894886d55ab2df68ce1e52cd041010
Oscar Silva <Oscar Silva>
2017-02-05 04:30:49 +0200

    automatical testing with midje does not work debugging is troublesome due to the test input not being visible when running through REPL

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,35 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
-
-(defn abs [x]
-  ":(")
-
-(defn divides? [divisor n]
-  ":(")
-
-(defn fizzbuzz [n]
-  ":(")
+  (if x true false))
 
 (defn teen? [age]
-  ":(")
+  (if (>= age 13) (<= age 19) false))
+
+(defn abs [x]
+(if (<= x 0) (- x) x))
+
+(defn divides? [divisor n]
+  (== 0 (mod n divisor)))
+
+(defn fizzbuzz [n]
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+  :else ""
+))
 
 (defn not-teen? [age]
-  ":(")
+(not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (if (divides? 400 year) true (and (divides? 4 year) (not (divides? 100 year))))
+)
