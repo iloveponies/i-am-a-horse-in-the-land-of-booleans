54ff3b10ed62b84744effe35546d4f89b7a97304
Jaakko Makilaurila <j.makilaurila@gmail.com>
2017-02-19 21:23:31 +0200

    Excercises completed

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -6,31 +6,43 @@
     false
     (if (= false x)
       false
-      true))
-  )
+      true)))
 
 (defn abs [x]
   (if (< x 0)
     (- x)
-    x)
-  )
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0)
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else           ""))
 
 (defn teen? [age]
   (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x)  nil
+    (list? x)   (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else       true))
 
 (defn leap-year? [year]
-  ":(")
+  (if (or (and (divides? 4 year) (not (divides? 100 year)))
+          (and (divides? 4 year) (and (divides? 100 year) (divides? 400 year))))
+    true
+    false))
 
 ; '_______'
