c4d76eca7a7ef282a171147d6a16c23448a5b431
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-03-26 12:15:14 +0300

    Implement generic-doublificate, fizzbuzz, leap-year

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -11,18 +11,36 @@
   (if (== (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""
+    )
+  )
 
 (defn teen? [age]
   (<= 13 age 19))
 
 (defn not-teen? [age]
-  (if (teen? age) false true))
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x) nil
+    (list? x) (* (count x) 2)
+    (vector? x) (* (count x) 2)
+    :else true
+    )
+  )
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 100 year) (divides? 400 year)
+    (divides? 4 year) true
+    :else false
+    )
+  )
 
 ; '_______'
