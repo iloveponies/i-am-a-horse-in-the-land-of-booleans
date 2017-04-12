559006afccc4226c1bc2a2bef67c8b906560a0ff
Miika Pärni <miika.parni@helsinki.fi>
2017-04-12 09:39:16 +0300

    I am a horse in the land of booleans commit 1

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,64 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+ (if (= x nil)
+    false
+    (if (= x false)
+      false
+      true
+    )
+  )
+)
 
 (defn abs [x]
-  ":(")
+   (cond
+   (> 0 x) (- x)
+   :else x)
+)
 
 (defn divides? [divisor n]
-  ":(")
+ (if (= 0 (mod n divisor))
+   true
+   false
+   )
+)
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (= true (divides? 15 n)) "gotcha!"
+    (= true (divides? 3 n)) "fizz"
+    (= true (divides? 5 n)) "buzz"
+  :else "")
+)
 
 (defn teen? [age]
-  ":(")
+ ( if (< 12 age 20)
+    true
+    false
+   )
+)
 
 (defn not-teen? [age]
-  ":(")
+  (cond
+    (= true (teen? age)) false
+    :else true)
+)
 
 (defn generic-doublificate [x]
-  ":(")
+    (cond
+      (number? x ) (* x 2)
+      (= true (empty? x)) nil
+      (= true (list? x)) (* 2(count x))
+      (= true (vector? x)) (* 2(count x))
+      :else true)
+)
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+      (not (= true (= 0 (mod year 4)))) false
+      (not (= true (= 0 (mod year 100)))) true
+      (not (= true (= 0 (mod year 400)))) false
+    :else true)
+)
 
 ; '_______'
