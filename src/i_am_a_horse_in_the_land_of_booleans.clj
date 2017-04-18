bab9a375b308c40ab628004dee349ff89fa7cce3
Henri Kontio <Henri.Kontio@solita.fi>
2017-04-18 11:33:26 +0300

    Add implmentation of abs and divides?

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -11,10 +11,14 @@
       true)))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
   ":(")
