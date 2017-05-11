456cec7d44d0cb170cae912f69121b45554eb862
Topi Pohjosaho <topi.pohjosaho@solita.fi>
2017-05-11 10:26:44 +0300

    booleans

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,47 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (neg? x) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (zero? (mod n divisor))
+    true
+    false))
+
+(defn not-divides? [divisor n]
+  (not (divides? divisor n)))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x)(vector? x)) (* 2 (count x))
+    :else true))
 
+; Write the function (leap-year? year), which returns true if year is a leap year, otherwise false.
+; A year is a leap year if it is divisible by 4, except if it is divisible by 100,
+; in which case it still is a leap year if it is divisible by 400.
 (defn leap-year? [year]
-  ":(")
+  (and
+   (divides? 4 year)
+   (or
+    (not-divides? 100 year)
+    (divides? 400 year))))
 
 ; '_______'
