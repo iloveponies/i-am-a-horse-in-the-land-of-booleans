c2c499062577f23c7eb726018b5123a95105a77f
Gautam Roy <gautam.2005@gmail.com>
2017-05-17 17:55:21 +0530

    completed ex5

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,24 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (= (mod n 3) 0) "fizz"
+    (= (mod n 5) 0) "buzz"
+    (= (mod n 15) 0) "gotcha!"
+    :else ""
+    ))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
   ":(")
