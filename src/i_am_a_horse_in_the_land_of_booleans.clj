90bbd86ef0b4ab724465c85a93e95cf3d69ba761
kilpsal <salla.kilpinen@gmail.com>
2017-06-13 20:13:49 +0300

    toka viikko

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,52 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
-
+ 
 (defn boolean [x]
-  ":(")
-
+  (if(or(false? x)(nil? x))
+    false
+    true))
+ 
 (defn abs [x]
-  ":(")
-
+  (if(< x 0)
+    (* -1 x)
+    x))
+ 
+ 
 (defn divides? [divisor n]
-  ":(")
-
+  (if(== 0 (rem n divisor))
+    true
+    false))
+ 
 (defn fizzbuzz [n]
-  ":(")
-
+  (cond
+   (divides? 15 n) "gotcha!"
+   (divides? 5 n)"buzz"
+   (divides? 3 n)"fizz"
+   :else ""))
+ 
+ 
 (defn teen? [age]
-  ":(")
-
+  (if(and(> age 12)(< age 20))
+    true
+    false))
+ 
 (defn not-teen? [age]
-  ":(")
-
+  (if(and(> age 12)(< age 20))
+    false
+    true))
+ 
 (defn generic-doublificate [x]
-  ":(")
-
+  (cond
+   (number? x)(* x 2)
+   (empty? x)nil
+   (or(vector? x)(list? x))(* 2 (count x))
+   :else true))
+ 
 (defn leap-year? [year]
-  ":(")
-
-; '_______'
+  (if (divides? 4 year)
+    (if(divides? 100 year)
+      (if(divides? 400 year)
+        true
+        false)
+      true)
+    false))
