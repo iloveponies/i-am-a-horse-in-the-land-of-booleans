e42c5b7018a5ca128977a0af20dae182e19da39a
sumire <eino.keningi@gmail.com>
2017-06-22 12:07:47 +0300

    Fix exercise 4, Exercise 5

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -8,10 +8,14 @@
   (if (< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  (if (== 0 (mod divisor n)) false true))
+  (if (== 0 (mod n divisor)) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else "" ))
 
 (defn teen? [age]
   (if (< 12 age 20) true false))
