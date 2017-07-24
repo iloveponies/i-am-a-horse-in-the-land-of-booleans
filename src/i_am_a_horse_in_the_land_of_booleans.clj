bbf81b6d34bd4f6bda9cb83bad698d72fee44481
secundoego <secundoego@zoho.com>
2017-07-25 00:54:58 +0200

    First commit

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,23 +1,47 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean
+      [x]
+      (if x
+        "true"
+        "false"
+      )
+)
 
-(defn abs [x]
-  ":(")
+(defn teen?
+  [age]
 
-(defn divides? [divisor n]
-  ":(")
+  (if (and (<= 13 age) (<= age 19))
+    "true"
+    "false"
+    )
+  )
+
+(defn abs
+    [x]
+    (if (< x 0)
+      (- x)
+      x
+  )
+)
+
+(defn divides?
+  [divisor n]
+  ( if (= 0 (mod n divisor))
+    "true"
+    "false"
+  )
+)
 
 (defn fizzbuzz [n]
   ":(")
 
-(defn teen? [age]
-  ":(")
-
 (defn not-teen? [age]
   ":(")
+
+
+
 
 (defn generic-doublificate [x]
   ":(")
