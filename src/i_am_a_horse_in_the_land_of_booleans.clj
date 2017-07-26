b19377dae4f34a35ab427f626f3d71738259b325
secundoego <secundoego@zoho.com>
2017-07-26 21:18:26 +0200

    Commit for push

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -4,8 +4,8 @@
 (defn boolean
       [x]
       (if x
-        "true"
-        "false"
+        true
+        false
       )
 )
 
@@ -13,8 +13,8 @@
   [age]
 
   (if (and (<= 13 age) (<= age 19))
-    "true"
-    "false"
+    true
+    false
     )
   )
 
@@ -29,24 +29,38 @@
 (defn divides?
   [divisor n]
   ( if (= 0 (mod n divisor))
-    "true"
-    "false"
+    true
+    false
   )
 )
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (== (mod n 15) 0) "gotcha!"
+    (== (mod n 5) 0) "buzz"
+    (== (mod n 3) 0) "fizz"
+    :else       ""))
 
 (defn not-teen? [age]
-  ":(")
-
-
-
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else       true
+  )
+)
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (== (mod year 400) 0) true
+    (== (mod year 100) 0) false
+    (== (mod year 4) 0) true
+    :else false
+  )
+)
 
 ; '_______'
