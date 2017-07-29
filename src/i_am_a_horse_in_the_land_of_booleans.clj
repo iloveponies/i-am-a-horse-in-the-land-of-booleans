4ed673f9b3c03011095c2057ea1c79e6bcda2690
jheiska <jaakkoheiska@gmail.com>
2017-07-29 21:01:04 +0300

    lähtihän se rullaamaan

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,25 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  if((= x nil) false true))
+  (if(= x nil) false (if (= x false) false true)))
 
 (defn abs [x]
-  ":(")
+  (if(< x 0) (- x) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if(== 0 (mod n divisor)) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 5 n) "buzz"
+    (divides? 3 n) "fizz"
+    :else ""
+  ))
+
 
 (defn teen? [age]
-  ":(")
+  (if(< 12 age 20) true false))
 
 (defn not-teen? [age]
   ":(")
@@ -26,3 +32,5 @@
   ":(")
 
 ; '_______'
+
+;
