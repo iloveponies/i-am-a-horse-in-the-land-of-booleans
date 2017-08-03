42820d9855d048b8fea5fb8ed59cb2ec9a25381c
squonkbadger <tatianatassi19@gmail.com>
2017-08-04 00:08:04 +0300

    something something wrong

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,36 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x] (if x true false))
+(defn boolean
+  [x]
+  (if x true false))
 
-(defn abs [x] (if (< x 0) (- x) x))
+(defn abs
+  [x]
+  (if (< x 0) (- x) x))
 
 (defn divides?
   [divisor n]
   (= 0 (mod n divisor)))
 
-(defn fizzbuzz [n]
+(defn fizzbuzz
+  [n]
   (cond
     (= 0 (mod n 15)) "gotcha!"
     (= 0 (mod n 5)) "buzz"
     (= 0 (mod n 3)) "fizz"
     :else ""))
 
-(defn teen? [age] (<= 13 age 19))
+(defn teen?
+  [age]
+  (<= 13 age 19))
 
-(defn not-teen? [age]
-  (if (teen? age) false true)
-  )
+(defn not-teen?
+  [age]
+  (if (teen? age) false true))
 
-(defn generic-doublificate [x]
+(defn generic-doublificate
+  [x]
   (cond
     (number? x) (* 2 x)
     (empty? x) nil
@@ -31,7 +39,8 @@
     :else true
     ))
 
-(defn leap-year? [year]
+(defn leap-year?
+  [year]
   (cond
     (not (== 0 (mod year 4))) false
     (not (== 0 (mod year 100))) true
