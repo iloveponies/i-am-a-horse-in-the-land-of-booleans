870c26a0c39ba24e2d8b6b5bcadc9c90ae9b7bb1
Jani-Matti Tirkkonen <janimat@T500.Home>
2017-08-07 01:14:31 +0300

    Completed exercises 1-8

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,56 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
+;; exercise 1.
+;; Implementing a boolean function
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
+;; exercise 3.
+;; absolute value
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x))
 
+;; exercise 4
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor))
+  true
+  false))
 
+;; exercise 5
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
+;; exercise 2.
+;; Are they a teenager?
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
+;; exercise 7
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
+;; exercise 6
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else true))
 
+;; exercise 8
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false))
 
-; '_______'
+                                        ; '_______'
