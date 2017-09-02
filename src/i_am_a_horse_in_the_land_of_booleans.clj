f1d9938c96dc5b87180c316a300e4d7e0961b93b
glmcguire <glmcguir@gmail.com>
2017-09-01 17:30:37 -0700

    Just past fizzbuzz. WIP

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,20 +1,46 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean
+  "Since nil and false return false in clojure,
+   a simple if statement evaluating the parameter
+   x will return the true or false needed to re-
+   implement the boolean function."
+  [x]
+  (if x true false))
+
 
 (defn abs [x]
-  ":(")
+  "Returns the absolute value of x by checking if it
+  is less than zero. If it is less than zero, it returns
+  (* -1 x), if it isn't, it returns x."
+  (if (< x 0)
+    (* -1 x)
+    x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides?
+  "Returns true if divisor divides n and false otherwise."
+  [divisor n]
+  (if (= 0 (mod n divisor))
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 3 n)
+    (if
+      (divides? 5 n) "gotchya!" "fizz")
+    (divides? 5 n)
+    (if
+      (divides? 3 n) "gotchya!" "buzz")
+    :else ""))
 
-(defn teen? [age]
-  ":(")
+(defn teen?
+  "Simple age comparator, indicating if
+   age parameter is a teen or not."
+  [age]
+  (< 12 age 20))
+
 
 (defn not-teen? [age]
   ":(")
