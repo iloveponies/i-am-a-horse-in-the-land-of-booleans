376e07e273421d1d4e306bd17437acecbdfccd72
Amit Adhikari <amit@amitadhikari.me>
2020-09-27 12:55:48 -0500

    second exercise

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,52 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or (= x nil) (= x false))
+    false
+    true))
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (* -1 x)
+    x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0)
+    true
+    false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
 (defn teen? [age]
-  ":(")
+  (if (and (<= age 19) (>= age 13))
+    true
+    false))
 
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age)))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x)  nil
+    (or (list? x) (vector? x)) (* (count x) 2)
+    :else true))
+
+
+(defn is-divisible [a b]
+  (= (mod a b) 0))
 
 (defn leap-year? [year]
-  ":(")
+  (cond
+    (not (is-divisible year 4)) false
+    (not (is-divisible year 100)) true
+    (not (is-divisible year 400)) false
+    :else true))
+
 
 ; '_______'
