de23944d8a28448b5f32a1e3c6b12b8dedb2ddf8
Piia Aro <ext-piia.1.aro@sok.fi>
2023-05-29 12:11:58 +0300

    initial commit

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,27 +2,43 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or (= x nil) (= x false))
+    false
+    true)) ;; OK
 
-(defn abs [x]
-  ":(")
-
+(defn abs [n]
+  (max n (- n)))
+;; (abs -5) OK
 (defn divides? [divisor n]
-  ":(")
+  (if (== (mod n divisor) 0) true false))
+ ;; OK
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz
+  [n]
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 5 n) "buzz"
+    (divides? 3 n) "fizz"
+    :else ""))
+
 
 (defn teen? [age]
-  ":(")
-
+  (if (and (>= age 13) (<= age 19))
+    true false)) ;; OK
 (defn not-teen? [age]
-  ":(")
+  (not (teen? age))) ;; ok
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (and (seqable? x) (empty? x)) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
-
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else false))
 ; '_______'
